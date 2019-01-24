9eb802b7fbef4d549cdf7e26b96cb02a336c4196
devanshu-m <devanshu_m@gwmail.gwu.edu>
2019-01-24 14:38:12 -0500

    Adds trackable columns to users table
    These columns are required when logging in via shib or local dev. Also updates readme to export database_auth variable.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190124162359) do
+ActiveRecord::Schema.define(version: 20190124182020) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -528,6 +528,11 @@ ActiveRecord::Schema.define(version: 20190124162359) do
     t.string "provider"
     t.string "uid"
     t.string "ppid"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid"], name: "index_users_on_uid"
