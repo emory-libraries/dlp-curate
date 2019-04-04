7dfa0a9fcec47a5b8701f578479e863ec2340b75
Alex Zotov <alexandr.zotov@emory.edu>
2019-04-04 10:57:38 -0400

    Ensures Hyrax supports multi-part objects

--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe FileSet do
-  it "has tests" do
-    skip "Add your tests here"
+  describe '#related_files' do
+    let!(:f1) { described_class.new }
+
+    context 'when there are no related files' do
+      it 'returns an empty array' do
+        expect(f1.related_files).to eq []
+      end
+    end
+
+    context 'when there are related files' do
+      let(:parent_work)   { FactoryBot.create(:work_with_files) }
+      let(:f1)            { parent_work.file_sets.first }
+      let(:f2)            { parent_work.file_sets.last }
+      let(:related_files) { f1.reload.related_files }
+
+      it 'returns all files contained in parent work(s) but excludes itself' do
+        expect(related_files).to include(f2)
+        expect(related_files).not_to include(f1)
+      end
+    end
   end
 end
