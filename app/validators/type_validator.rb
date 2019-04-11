4c9458b5880d7fab55cd1023b7885a5e81d17220
Alex Zotov <alexandr.zotov@emory.edu>
2019-04-11 11:10:11 -0400

    fixing validators

--- app/validators/type_validator.rb
+++ app/validators/type_validator.rb
@@ -1,6 +1,6 @@
 class TypeValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    return unless value.blank?
+    return if value.blank?
     record.errors.add attribute, (options[:message] || "is not of class #{options[:with]}") unless
     value.class == options[:with]
   end
