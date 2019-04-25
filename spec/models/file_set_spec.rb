1c812df9164dd60373b8bef1dc343df0fd490edc
Alex Zotov <alexandr.zotov@emory.edu>
2019-04-25 15:46:43 -0400

    Ensures Hyrax supports multi-part objects (#175)

--- spec/models/file_set_spec.rb
+++ spec/models/file_set_spec.rb
@@ -1,6 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe FileSet do
+  describe '#related_files' do
+    let!(:f1) { described_class.new }
+
+    context 'when there are no related files' do
+      it 'returns an empty array' do
+        expect(f1.related_files).to eq []
+      end
+    end
+
+    context 'when there are related files' do
+      let(:parent_work)   { FactoryBot.create(:work_with_files) }
+      let(:f1)            { parent_work.file_sets.first }
+      let(:f2)            { parent_work.file_sets.last }
+      let(:related_files) { f1.reload.related_files }
+
+      it 'returns all files contained in parent work(s) but excludes itself' do
+        expect(related_files).to include(f2)
+        expect(related_files).not_to include(f1)
+      end
+    end
+  end
+
   it "multiple pcdm_use error" do
     expect { described_class.new(pcdm_use: [described_class::PRIMARY]) }.to raise_error(ArgumentError)
   end
@@ -9,16 +31,16 @@ RSpec.describe FileSet do
       subject(:file_set) { described_class.new(pcdm_use: described_class::PRIMARY) }
       its(:pcdm_use) { is_expected.to eq described_class::PRIMARY }
       it { is_expected.to be_primary }
-      it { is_expected.not_to be_supplemental }
+      it { is_expected.not_to be_supplementary }
     end
   end
 
-  describe "#supplemental" do
-    context 'when supplemental' do
+  describe "#supplementary" do
+    context 'when supplementary' do
       subject(:file_set) { described_class.new(pcdm_use: described_class::SUPPLEMENTAL) }
       its(:pcdm_use) { is_expected.to eq described_class::SUPPLEMENTAL }
       it { is_expected.not_to be_primary }
-      it { is_expected.to be_supplemental }
+      it { is_expected.to be_supplementary }
     end
   end
 end
