b3ca404d03d7d12a25b1c788b342560a467c591e
Alex Zotov <alexandr.zotov@emory.edu>
2019-04-30 13:38:52 -0400

    adds feature test to edit file label

---
+++ spec/features/edit_file_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe "Showing a file:", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  let(:file_title) { 'Some kind of title' }
+  let(:work) { FactoryBot.build(:work, user: user) }
+  let(:file_set) { FactoryBot.create(:file_set, user: user, title: [file_title]) }
+  let(:file) { File.open(fixture_path + '/sun.png') }
+
+  before do
+    login_as user
+    Hydra::Works::AddFileToFileSet.call(file_set, file, :original_file)
+    work.ordered_members << file_set
+    work.save!
+  end
+
+  context 'when the user tries to update file label' do
+    it 'shows the edit page again' do
+      visit edit_hyrax_file_set_path(file_set)
+      click_link 'Descriptions'
+      fill_in('Title', with: 'My Test Work')
+      find('input[name="commit"]').click
+    end
+  end
+end
