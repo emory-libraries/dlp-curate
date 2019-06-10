212042d28f5c29832217d8818a3f5a0e8ff078ba
Alex Zotov <alexandr.zotov@emory.edu>
2019-06-10 13:15:41 -0400

    Adds link to Parent work on a Fileset's show page

--- app/controllers/hyrax/curate_generic_works_controller.rb
+++ app/controllers/hyrax/curate_generic_works_controller.rb
@@ -10,5 +10,30 @@ module Hyrax
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::CurateGenericWorkPresenter
+
+    def show # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      @user_collections = user_collections
+      @parent = main_app.polymorphic_path(parent_presenter) if parent_presenter
+
+      respond_to do |wants|
+        wants.html { presenter && parent_presenter }
+        wants.json do
+          # load and authorize @curation_concern manually because it's skipped for html
+          @curation_concern = _curation_concern_type.find(params[:id]) unless curation_concern
+          authorize! :show, @curation_concern
+          render :show, status: :ok
+        end
+        additional_response_formats(wants)
+        wants.ttl do
+          render body: presenter.export_as_ttl, content_type: 'text/turtle'
+        end
+        wants.jsonld do
+          render body: presenter.export_as_jsonld, content_type: 'application/ld+json'
+        end
+        wants.nt do
+          render body: presenter.export_as_nt, content_type: 'application/n-triples'
+        end
+      end
+    end
   end
 end
