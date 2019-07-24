d82d1040b3ddf025d53b8e7e71d1b763b10f8adc
Bess Sadler <bess@curationexperts.com>
2019-07-24 16:47:00 -0400

    Allow database auth in production mode

--- app/models/auth_config.rb
+++ app/models/auth_config.rb
@@ -3,6 +3,6 @@ class AuthConfig
   # but in development mode, you may want to use local database
   # authentication instead.
   def self.use_database_auth?
-    !Rails.env.production? && ENV['DATABASE_AUTH'] == 'true'
+    ENV['DATABASE_AUTH'] == 'true'
   end
 end
