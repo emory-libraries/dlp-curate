634e06b3eeccd0e92b49183a1dd2539275b6baef
Alex Zotov <alexandr.zotov@emory.edu>
2019-08-05 15:45:23 -0400

    adds parent child relationship on view page

---
+++ app/services/parent_query_service.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+module ParentQueryService
+  def self.query_parents_for_id(child_id)
+    ActiveFedora::SolrService.get("member_ids_ssim:#{child_id}", rows: 1000)["response"]["docs"]
+  end
+end
