e68021bc2caf358f5b2897c6da53a65bf5f9cf2a
Bess Sadler <bess@curationexperts.com>
2019-08-06 14:22:56 -0400

    Remove unused files

--- config/initializers/prepends.rb
+++ config/initializers/prepends.rb
@@ -1,5 +1,3 @@
 require_relative '../prepends/hyrax_record_importer_prepends'
-require_relative '../prepends/csv_manifest_validator_prepends'
 
 Zizia::HyraxRecordImporter.prepend(HyraxRecordImporterPrepends)
-Zizia::CsvManifestValidator.prepend(CsvManifestValidatorPrepends)
