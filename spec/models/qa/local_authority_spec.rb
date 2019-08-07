6923f7c709b6ef3291356e738079cdd3db5611cd
Bess Sadler <bess@curationexperts.com>
2019-08-07 15:53:39 -0400

    Bug fix: key not found: "active" (#420)
    This fixes a bug where the collection edit form would not render if
    there was a saved value in the administrative_unit field. The root cause
    of this was that the form wanted to filter on "active" QA values, and
    administrative_unit didn't have anything explicitly marked as "active".
    
    This PR adds explicit "active" designation to the administrative_unit QA
    config.
    
    It also adds a system spec for the collection edit form as well as some
    unit tests for QA configuration.

--- spec/models/qa/local_authority_spec.rb
+++ spec/models/qa/local_authority_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Qa::LocalAuthority, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'resource_types' do
+    it 'has active terms for everything' do
+      active_terms = Qa::Authorities::Local.subauthority_for('resource_types').all.select { |term| term[:active] }
+      expect(active_terms).not_to be_empty
+    end
+  end
+
+  context 'administrative_unit' do
+    it 'has active terms for everything' do
+      active_terms = Qa::Authorities::Local.subauthority_for('administrative_unit').all.select { |term| term[:active] }
+      expect(active_terms).not_to be_empty
+    end
+  end
 end
