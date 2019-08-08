3cab275692ac7231731bb6d427cc4a6e12bfe231
Jamie Little <jamie@jamielittle.org>
2019-08-08 13:45:15 -0500

    View metadata for work on show page
    This adds a class that can be used to get a list
    of attributes on the work. That is used to generate
    the `SolrDocument` methods, view, and catalog controller code
    that is needed to display metadata in the show page.
    
    All the metadata attributes that can be used currently
    are set in the work factory and then a system spec
    is used to make sure that the data is showing up on the page.
    
    This also includes addtions to the locale files so that the
    correct labels are displayed.

--- app/presenters/hyrax/curate_generic_work_presenter.rb
+++ app/presenters/hyrax/curate_generic_work_presenter.rb
@@ -2,5 +2,8 @@
 #  `rails generate hyrax:work CurateGenericWork`
 module Hyrax
   class CurateGenericWorkPresenter < Hyrax::WorkShowPresenter
+    CurateGenericWorkAttributes.instance.attributes.each do |key|
+      delegate key.to_sym, to: :solr_document
+    end
   end
 end
