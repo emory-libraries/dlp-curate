56ca110212596d62d8273cab921dffc333763b65
Bess Sadler <bess@curationexperts.com>
2019-08-09 14:52:12 -0400

    Ensure Curate::CollectionType has the right participants
    Automatically set up Curate::CollectionType to only allow admins to
    manage the collections, do not allow Registered Users to Create works in
    them.

--- spec/models/curate/curate_collection_type_spec.rb
+++ spec/models/curate/curate_collection_type_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
+# Goal: Easily create Collection objects that follow the template laid out by this
+# CollectionType created by Emily:
+# => #<Hyrax::CollectionType id: 3, title: "Library Collection",
+# description: "Library staff-curated collections", machine_id: "library_collection",
+# nestable: true, discoverable: true, sharable: true, allow_multiple_membership: false,
+# require_membership: false, assigns_workflow: false, assigns_visibility: false,
+# share_applies_to_new_works: true, brandable: true, badge_color: "#663333">
+
 RSpec.describe Curate::CollectionType, :clean, type: :model do
   let(:collection_type) { described_class.new }
 
@@ -11,8 +19,29 @@ RSpec.describe Curate::CollectionType, :clean, type: :model do
     expect(Curate::CollectionType.count).to eq 1
   end
 
+  it 'only allows users with the admin role to manage' do
+    # It does not have Registered Users group in the Creators role
+    ct = Curate::CollectionType.find_or_create_library_collection_type
+    collection_type_participants = Hyrax::CollectionTypeParticipant.where(hyrax_collection_type_id: ct.id)
+    expect(collection_type_participants.size).to eq 1
+    expect(collection_type_participants.first.agent_id).to eq "admin"
+    expect(collection_type_participants.first.access).to eq "manage"
+  end
+
   it "is named Library Collection" do
     expect(collection_type.title).to eq "Library Collection"
+  end
+
+  it 'has a description' do
+    expect(collection_type.description).to eq "Library staff-curated collections"
+  end
+
+  it 'has a machine_id' do
+    expect(collection_type.machine_id).to eq "library_collection"
+  end
+
+  it 'has a specific badge color' do
+    expect(collection_type.badge_color).to eq "#663333"
   end
 
   it "has nesting enabled" do
