fa62fc716b951297ed96509b90d33ad652e9dde8
Bess Sadler <bess@curationexperts.com>
2019-08-09 15:06:31 -0400

    Refactor so we don't have to delete existing CollectionTypes
    Many curate systems already have a Library Collection CollectionType
    now, so let's make life easier for ourselves and just update those
    instead of having to blow everything away and re-create it.

--- app/models/curate/collection_type.rb
+++ app/models/curate/collection_type.rb
@@ -1,22 +1,34 @@
 module Curate
   class CollectionType < Hyrax::CollectionType
-    # If a Curate::CollectionType already exists, return it.
+    # If a Curate::CollectionType already exists, ensure it adheres to expectations and return it.
     # Otherwise, make a new one and return that.
     def self.find_or_create_library_collection_type
       existing_library_collection_type = Curate::CollectionType.find_by_title("Library Collection")
-      return existing_library_collection_type if existing_library_collection_type
+      if existing_library_collection_type
+        existing_library_collection_type.configure
+        return existing_library_collection_type
+      end
       new_library_collection_type = Curate::CollectionType.new
-      new_library_collection_type.save
       new_library_collection_type
     end
 
     def initialize
       super
+      configure
+    end
+
+    def configure
       self.title = "Library Collection"
-      self.description = "Library staff-curated collections"
+      self.description = "Library staff curated collections"
       self.allow_multiple_membership = false
       save
+      remove_all_participants
       allow_admins_to_manage
+    end
+
+    # Remove all participants from the collection type and only add back in admins
+    def remove_all_participants
+      Hyrax::CollectionTypeParticipant.where(hyrax_collection_type_id: id).all.find_each(&:destroy!)
     end
 
     def allow_admins_to_manage
