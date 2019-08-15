96bffa8e531083166a5cd93f94657512438b5fb5
Jamie Little <jamie@jamielittle.org>
2019-08-15 12:28:51 -0500

    Fix for Collection visibility not saving
    The `visibility` property needs to be present in the `Form`
    class.

--- app/forms/hyrax/forms/collection_form.rb
+++ app/forms/hyrax/forms/collection_form.rb
@@ -27,7 +27,7 @@ module Hyrax
                     :subject_names, :subject_geo, :subject_time_periods, :note,
                     :rights_documentation, :sensitive_material, :internal_rights_note,
                     :contact_information, :staff_note, :system_of_record_ID, :legacy_ark,
-                    :primary_repository_ID]
+                    :primary_repository_ID, :visibility]
 
       self.required_fields = [:title, :holding_repository, :creator, :abstract, :primary_repository_ID]
 
