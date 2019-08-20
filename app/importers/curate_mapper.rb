21e85a9b294311334ed666a909ee7f56db471148
Bess Sadler <bess@curationexperts.com>
2019-08-20 13:20:35 -0400

    Required csv fields (#489)
    * Map import for administrative_unit
    
    * Alphabetize metadata fields
    
    * Map date_created field
    
    * Mappings for rights statements
    
    * Mapping for data_classification
    
    * Fix a few mapping errors

--- app/importers/curate_mapper.rb
+++ app/importers/curate_mapper.rb
@@ -4,12 +4,17 @@ class CurateMapper < Zizia::HashMapper
   attr_reader :row_number
 
   CURATE_TERMS_MAP = {
-    title: "title",
+    administrative_unit: "administrative_unit",
     content_type: "content_type",
+    data_classification: "data_classification",
+    date_created: "date_created",
+    rights_statement_text: "rights_statement_text",
+    rights_statement: "rights_statement",
+    title: "title",
     visibility: "visibility"
   }.freeze
 
-  DELIMITER = '|~|'
+  DELIMITER = '|'
 
   def initialize(attributes = {})
     @row_number = attributes[:row_number]
@@ -50,6 +55,41 @@ class CurateMapper < Zizia::HashMapper
     }.freeze
   end
 
+  # Normalize the value coming in because there are subtle mis-matches against the expected controlled
+  # vocabulary term. E.g.,
+  # "Stuart A. Rose Manuscript, Archives and Rare Book Library" vs
+  # "Stuart A. Rose Manuscript, Archives, and Rare Book Library"
+  def administrative_unit
+    active_terms = Qa::Authorities::Local.subauthority_for('administrative_unit').all.select { |term| term[:active] }
+    csv_term = @metadata["administrative_unit"]
+    normalized_csv_term = csv_term.downcase.gsub(/[^a-z0-9\s]/i, '')
+    valid_option = active_terms.select { |s| s["id"].downcase.gsub(/[^a-z0-9\s]/i, '') == normalized_csv_term }.try(:first)
+    return valid_option["id"] if valid_option
+    raise "Invalid administrative_unit value: #{csv_term}"
+  end
+
+  # Iterate through all values for data_classification and ensure they are all
+  # valid options according to Questioning Authority
+  def data_classification
+    csv_terms = @metadata["data_classification"]&.split(DELIMITER)
+    active_terms = Qa::Authorities::Local.subauthority_for('data_classification').all.select { |term| term[:active] }
+    data_classification_values = []
+    csv_terms.each do |c|
+      valid_option = active_terms.select { |s| s["id"] == c }.try(:first)
+      raise "Invalid data_classification value: #{c}" unless valid_option
+      data_classification_values << valid_option["id"]
+    end
+    data_classification_values
+  end
+
+  def rights_statement
+    active_terms = Qa::Authorities::Local.subauthority_for('rights_statements').all.select { |term| term[:active] }
+    csv_term = @metadata["rights_statement"]
+    valid_uri_option = active_terms.select { |s| s["id"] == csv_term }.try(:first)
+    return csv_term if valid_uri_option
+    raise "Invalid rights_statement value: #{csv_term}"
+  end
+
   # If we get a URI for content_type, check that it matches a URI in the questioning
   # authority config, and return it if so.
   # If we get a string for content_type, (e.g., 'still image'), transform it into its
@@ -66,8 +106,13 @@ class CurateMapper < Zizia::HashMapper
     matching_term["id"]
   end
 
+  def singular_fields
+    ["date_created"]
+  end
+
   def map_field(name)
     return unless CURATE_TERMS_MAP.keys.include?(name)
+    return @metadata[name.to_s] if singular_fields.include?(name.to_s)
 
     Array.wrap(CURATE_TERMS_MAP[name]).map do |source_field|
       metadata[source_field]&.split(DELIMITER)
--- spec/importers/curate_mapper_spec.rb
+++ spec/importers/curate_mapper_spec.rb
@@ -7,8 +7,13 @@ RSpec.describe CurateMapper do
 
   let(:metadata) do
     {
-      "title" => "what an awesome title", # title
+      "administrative_unit" => "Stuart A. Rose Manuscript, Archives and Rare Book Library",
       "content_type" => 'still image',
+      "data_classification" => "Confidential|Internal",
+      "date_created" => "1985-11-01",
+      "rights_statement_text" => "Emory University does not control copyright for this image.",
+      "rights_statement" => "http://rightsstatements.org/vocab/InC/1.0/",
+      "title" => "what an awesome title",
       "visibility" => "Emory Network"
     }
   end
@@ -19,18 +24,9 @@ RSpec.describe CurateMapper do
     expect(Zizia.config.metadata_mapper_class).to eq described_class
   end
 
-  context "#visibility" do
-    context "Emory Network" do
-      let(:metadata) do
-        {
-          "title" => "my title",
-          "content_type" => "http://id.loc.gov/vocabulary/resourceTypes/img",
-          "visibility" => "Emory Network"
-        }
-      end
-      it "gives authenticated" do
-        expect(mapper.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
-      end
+  context "#administrative_unit" do
+    it "does its best to match the configured controlled vocabulary term" do
+      expect(mapper.administrative_unit).to eq "Stuart A. Rose Manuscript, Archives, and Rare Book Library"
     end
   end
 
@@ -70,9 +66,61 @@ RSpec.describe CurateMapper do
     end
   end
 
-  it "maps the required title field" do
-    expect(mapper.map_field(:title))
-      .to contain_exactly("what an awesome title")
+  context "#data_classification" do
+    it "maps the data_classification field" do
+      expect(mapper.data_classification).to contain_exactly("Confidential", "Internal")
+    end
+  end
+
+  context "#date_created" do
+    it "maps the date_created field" do
+      expect(mapper.date_created).to eq "1985-11-01"
+    end
+  end
+
+  context "#rights_statement_text" do
+    it "maps the rights_statement_text field" do
+      expect(mapper.rights_statement_text)
+        .to contain_exactly("Emory University does not control copyright for this image.")
+    end
+  end
+
+  context "#rights_statement" do
+    it "maps the rights_statement field when it's valid" do
+      expect(mapper.rights_statement).to eq "http://rightsstatements.org/vocab/InC/1.0/"
+    end
+    context "invalid rights statement" do
+      let(:metadata) do
+        {
+          "rights_statement" => "http://badrightsstatements.org/vocab/InC/1.0/"
+        }
+      end
+      it "raises an exception when it isn't valid" do
+        expect { mapper.rights_statement }.to raise_error RuntimeError
+      end
+    end
+  end
+
+  context "#title" do
+    it "maps the required title field" do
+      expect(mapper.map_field(:title))
+        .to contain_exactly("what an awesome title")
+    end
+  end
+
+  context "#visibility" do
+    context "Emory Network" do
+      let(:metadata) do
+        {
+          "title" => "my title",
+          "content_type" => "http://id.loc.gov/vocabulary/resourceTypes/img",
+          "visibility" => "Emory Network"
+        }
+      end
+      it "gives authenticated" do
+        expect(mapper.visibility).to eq Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_AUTHENTICATED
+      end
+    end
   end
 
   describe '#fields' do
