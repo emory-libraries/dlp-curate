9b3f94cd6e2d52f88e69e7b5704ba5c545a05ef9
Bess Sadler <bess@curationexperts.com>
2019-08-22 13:22:55 -0400

    Standardize the creation of default admin set in tests

--- spec/system/create_collection_spec.rb
+++ spec/system/create_collection_spec.rb
@@ -3,16 +3,11 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.describe 'Creating a collection', :perform_jobs, clean: true, type: :system, js: true do
+RSpec.describe 'Creating a collection', :perform_jobs, clean: true, admin_set: true, type: :system, js: true do
   context 'logged in as an admin user' do
-    let(:admin_set_id) { AdminSet.find_or_create_default_admin_set_id }
-    let(:permission_template) { Hyrax::PermissionTemplate.find_or_create_by!(source_id: admin_set_id) }
     let(:admin_user) { FactoryBot.create(:admin) }
 
     before do
-      admin_set_id
-      permission_template
-      Hyrax::CollectionType.find_or_create_default_collection_type
       login_as admin_user
     end
 
