0294c9d369440d24a0af144a7c7c1bf5f581b5d5
Bess Sadler <bess@curationexperts.com>
2019-08-23 13:29:42 -0400

    Configure logging for import
    * Standardize on logs that display timestamp and severity
    * Set log level to INFO -- DEBUG is too chatty and makes it harder to
    find the signal in all that noise

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative '../app/logging/log_formatter'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,7 +13,7 @@ module DlpCurate
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.logger = ActiveSupport::Logger.new("log/#{Rails.env}.log")
+    config.log_formatter = LogFormatter.new
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
+Rails.logger.level = Logger::INFO
