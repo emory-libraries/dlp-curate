be31de78fdaa705e6d0efd6de3ce577307ad1e19
Jamie Little <jamie@jamielittle.org>
2019-08-23 12:26:00 -0500

    Add whether a field is required on the form to guide
    This adds the required field on form property to
    the guide and csv.

--- app/lib/metadata_details.rb
+++ app/lib/metadata_details.rb
@@ -4,7 +4,7 @@ require 'csv'
 class MetadataDetails
   include Singleton
 
-  def details(work_attributes:)
+  def details(work_attributes:) # rubocop:disable Metrics/AbcSize
     validators = work_attributes.validators
     work_attributes.properties.sort.map do |p|
       Hash[
@@ -15,7 +15,8 @@ class MetadataDetails
         type: type_to_s(p[1].type),
         validator: validator_to_string(validator: validators[p[0].to_sym][0]),
         label: I18n.t("simple_form.labels.defaults.#{p[0]}"),
-        csv_header: csv_header(p[0])
+        csv_header: csv_header(p[0]),
+        required_on_form: required_on_form_to_s(p[0]),
       ]
     end.reduce({}, :merge)
   end
@@ -36,6 +37,10 @@ class MetadataDetails
 
     def csv_header(field)
       Zizia.config.metadata_mapper_class.csv_header(field) || "not configured"
+    end
+
+    def required_on_form_to_s(attribute)
+      REQUIRED_FIELDS_ON_FORM.include?(attribute.to_sym).to_s
     end
 
     def type_to_s(type)
---
+++ config/initializers/required_fields_on_form.rb
@@ -0,0 +1,2 @@
+REQUIRED_FIELDS_ON_FORM = [:title, :holding_repository, :content_type, :rights_statement_text, :rights_statement,
+                           :data_classification, :date_created].freeze
