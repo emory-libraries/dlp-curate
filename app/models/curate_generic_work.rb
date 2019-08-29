b82868ce2d9f524747f2afdb5517d9ccce99c235
Collin Brittle <cnbrittle@gmail.com>
2019-08-29 18:52:43 +0000

    Alphabetizes the CurateGenericWork properties
    When checking for the names and presence of properties, we don't care
    about the order, so use match_array rather than eq.

--- app/models/curate_generic_work.rb
+++ app/models/curate_generic_work.rb
@@ -16,75 +16,25 @@ class CurateGenericWork < ActiveFedora::Base
   validates :related_datasets, url: true, if: -> { related_datasets.present? }
   validates :rights_documentation, url: true, if: -> { rights_documentation.present? }
 
-  property :preservation_workflow, predicate: "http://metadata.emory.edu/vocab/cor-terms#preservation_workflow", class_name: "PreservationWorkflow"
-
-  property :institution, predicate: "http://rdaregistry.info/Elements/u/P60402", multiple: false do |index|
+  property :abstract, predicate: "http://purl.org/dc/elements/1.1/description", multiple: false do |index|
     index.as :stored_searchable
   end
 
-  property :holding_repository, predicate: "http://id.loc.gov/vocabulary/relators/rps", multiple: false do |index|
-    index.as :stored_searchable, :facetable
+  property :access_right, predicate: "http://purl.org/dc/terms/accessRights" do |index|
+    index.as :stored_searchable
   end
 
   property :administrative_unit, predicate: "http://id.loc.gov/vocabulary/relators/cur", multiple: false do |index|
     index.as :stored_searchable
   end
 
-  property :sublocation, predicate: "http://metadata.emory.edu/vocab/cor-terms#physicalSublocation", multiple: false do |index|
+  property :author_notes, predicate: "http://metadata.emory.edu/vocab/cor-terms#authorNote", multiple: false do |index|
     index.as :stored_searchable
   end
 
-  property :content_type, predicate: "http://purl.org/dc/terms/type", multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :content_genre, predicate: "http://www.europeana.edu/schemas/edm/hasType" do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :abstract, predicate: "http://purl.org/dc/elements/1.1/description", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :table_of_contents, predicate: "http://purl.org/dc/terms/tableOfContents", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :edition, predicate: "http://id.loc.gov/ontologies/bibframe/editionStatement", multiple: false
-
-  property :primary_language, predicate: "http://purl.org/dc/elements/1.1/language", multiple: false do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :subject_topics, predicate: "http://purl.org/dc/elements/1.1/subject" do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :subject_names, predicate: "http://purl.org/dc/elements/1.1/subject#names" do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :subject_geo, predicate: "http://purl.org/dc/elements/1.1/coverage" do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :subject_time_periods, predicate: "http://schema.org/temporalCoverage" do |index|
-    index.as :stored_searchable
-  end
+  property :conference_dates, predicate: "http://rdaregistry.info/Elements/u/P60526", multiple: false
 
   property :conference_name, predicate: "http://purl.org/dc/terms/relation#conferenceOrMeeting", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :uniform_title, predicate: "http://purl.org/dc/elements/1.1/title", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :series_title, predicate: "http://id.loc.gov/ontologies/bibframe/seriesStatement", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :parent_title, predicate: "http://rdaregistry.info/Elements/u/P60101", multiple: false do |index|
     index.as :stored_searchable
   end
 
@@ -92,11 +42,11 @@ class CurateGenericWork < ActiveFedora::Base
     index.as :stored_searchable
   end
 
-  property :publisher_version, predicate: "http://metadata.emory.edu/vocab/cor-terms#publicationStage", multiple: false do |index|
-    index.as :stored_searchable
+  property :content_genre, predicate: "http://www.europeana.edu/schemas/edm/hasType" do |index|
+    index.as :stored_searchable, :facetable
   end
 
-  property :creator, predicate: "http://purl.org/dc/elements/1.1/creator" do |index|
+  property :content_type, predicate: "http://purl.org/dc/terms/type", multiple: false do |index|
     index.as :stored_searchable, :facetable
   end
 
@@ -104,9 +54,51 @@ class CurateGenericWork < ActiveFedora::Base
     index.as :stored_searchable
   end
 
-  property :sponsor, predicate: "http://id.loc.gov/vocabulary/relators/spn", multiple: false
+  property :copyright_date, predicate: "http://purl.org/dc/terms/dateCopyrighted", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :creator, predicate: "http://purl.org/dc/elements/1.1/creator" do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :data_classification, predicate: "http://metadata.emory.edu/vocab/cor-terms#dataClassification" do |index|
+    index.as :stored_searchable
+  end
+
+  property :data_collection_dates, predicate: "http://metadata.emory.edu/vocab/cor-terms#dataCollectionDates" do |index|
+    index.as :stored_searchable
+  end
 
   property :data_producer, predicate: "http://id.loc.gov/vocabulary/relators/prv" do |index|
+    index.as :stored_searchable
+  end
+
+  property :data_source_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#dataSourcesNote" do |index|
+    index.as :stored_searchable
+  end
+
+  property :date_created, predicate: "http://purl.org/dc/terms/created", multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :date_digitized, predicate: "http://metadata.emory.edu/vocab/cor-terms#dateDigitized", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :date_issued, predicate: "http://purl.org/dc/terms/issued", multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :edition, predicate: "http://id.loc.gov/ontologies/bibframe/editionStatement", multiple: false
+
+  property :extent, predicate: "http://www.rdaregistry.info/Elements/u/#P60550", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :final_published_version, predicate: "http://purl.org/dc/terms/hasVersion"
+
+  property :geographic_unit, predicate: "http://metadata.emory.edu/vocab/cor-terms#geographicUnit", multiple: false do |index|
     index.as :stored_searchable
   end
 
@@ -116,99 +108,11 @@ class CurateGenericWork < ActiveFedora::Base
 
   property :grant_information, predicate: "http://metadata.emory.edu/vocab/cor-terms#grantOrFundingNote"
 
-  property :author_notes, predicate: "http://metadata.emory.edu/vocab/cor-terms#authorNote", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :note, predicate: "http://www.w3.org/2004/02/skos/core#note" do |index|
-    index.as :stored_searchable
-  end
-
-  property :data_source_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#dataSourcesNote" do |index|
-    index.as :stored_searchable
-  end
-
-  property :geographic_unit, predicate: "http://metadata.emory.edu/vocab/cor-terms#geographicUnit", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :technical_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#technicalNote", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :issn, predicate: "http://id.loc.gov/vocabulary/identifiers/issn", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :isbn, predicate: "http://id.loc.gov/vocabulary/identifiers/isbn", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :related_publications, predicate: "http://purl.org/dc/terms/relation#publication"
-  property :related_datasets, predicate: "http://purl.org/dc/terms/relation#dataset"
-  property :extent, predicate: "http://www.rdaregistry.info/Elements/u/#P60550", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :date_issued, predicate: "http://purl.org/dc/terms/issued", multiple: false do |index|
+  property :holding_repository, predicate: "http://id.loc.gov/vocabulary/relators/rps", multiple: false do |index|
     index.as :stored_searchable, :facetable
   end
 
-  property :conference_dates, predicate: "http://rdaregistry.info/Elements/u/P60526", multiple: false
-
-  property :data_collection_dates, predicate: "http://metadata.emory.edu/vocab/cor-terms#dataCollectionDates" do |index|
-    index.as :stored_searchable
-  end
-
-  property :local_call_number, predicate: "http://metadata.emory.edu/vocab/cor-terms#localCallNumber", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :related_material, predicate: "http://purl.org/dc/elements/1.1/relation" do |index|
-    index.as :stored_searchable
-  end
-
-  property :final_published_version, predicate: "http://purl.org/dc/terms/hasVersion"
-
-  property :issue, predicate: "http://purl.org/ontology/bibo/issue", multiple: false
-
-  property :page_range_start, predicate: "http://purl.org/ontology/bibo/pageStart", multiple: false
-
-  property :page_range_end, predicate: "http://purl.org/ontology/bibo/pageEnd", multiple: false
-
-  property :volume, predicate: "http://purl.org/ontology/bibo/volume", multiple: false
-
-  property :place_of_production, predicate: "http://id.loc.gov/vocabulary/relators/pup", multiple: false
-
-  property :keywords, predicate: "http://schema.org/keywords" do |index|
-    index.as :stored_searchable
-  end
-
-  property :rights_statement_text, predicate: "http://purl.org/dc/elements/1.1/rights" do |index|
-    index.as :stored_searchable
-  end
-
-  property :rights_statement, predicate: "http://www.europeana.eu/schemas/edm/rights" do |index|
-    index.as :stored_searchable, :facetable
-  end
-
-  property :rights_holder, predicate: "http://purl.org/dc/terms/rightsHolder"
-
-  property :copyright_date, predicate: "http://purl.org/dc/terms/dateCopyrighted", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :access_right, predicate: "http://purl.org/dc/terms/accessRights" do |index|
-    index.as :stored_searchable
-  end
-
-  property :rights_documentation, predicate: "http://metadata.emory.edu/vocab/cor-terms#rightsDocumentationURI", multiple: false
-
-  property :scheduled_rights_review, predicate: "http://metadata.emory.edu/vocab/cor-terms#scheduledRightsReview", multiple: false do |index|
-    index.as :stored_searchable
-  end
-
-  property :scheduled_rights_review_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#scheduledRightsReviewNote", multiple: false do |index|
+  property :institution, predicate: "http://rdaregistry.info/Elements/u/P60402", multiple: false do |index|
     index.as :stored_searchable
   end
 
@@ -216,11 +120,95 @@ class CurateGenericWork < ActiveFedora::Base
     index.as :stored_searchable
   end
 
+  property :isbn, predicate: "http://id.loc.gov/vocabulary/identifiers/isbn", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :issn, predicate: "http://id.loc.gov/vocabulary/identifiers/issn", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :issue, predicate: "http://purl.org/ontology/bibo/issue", multiple: false
+
+  property :keywords, predicate: "http://schema.org/keywords" do |index|
+    index.as :stored_searchable
+  end
+
+  property :legacy_ark, predicate: "http://id.loc.gov/vocabulary/identifiers/local#ark" do |index|
+    index.as :stored_searchable
+  end
+
+  property :legacy_identifier, predicate: "http://id.loc.gov/vocabulary/identifiers/local#legacy" do |index|
+    index.as :stored_searchable
+  end
+
   property :legacy_rights, predicate: "http://metadata.emory.edu/vocab/cor-terms#legacyRights", multiple: false do |index|
     index.as :stored_searchable
   end
 
-  property :data_classification, predicate: "http://metadata.emory.edu/vocab/cor-terms#dataClassification" do |index|
+  property :local_call_number, predicate: "http://metadata.emory.edu/vocab/cor-terms#localCallNumber", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :note, predicate: "http://www.w3.org/2004/02/skos/core#note" do |index|
+    index.as :stored_searchable
+  end
+
+  property :page_range_end, predicate: "http://purl.org/ontology/bibo/pageEnd", multiple: false
+
+  property :page_range_start, predicate: "http://purl.org/ontology/bibo/pageStart", multiple: false
+
+  property :parent_title, predicate: "http://rdaregistry.info/Elements/u/P60101", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :place_of_production, predicate: "http://id.loc.gov/vocabulary/relators/pup", multiple: false
+
+  property :preservation_workflow, predicate: "http://metadata.emory.edu/vocab/cor-terms#preservation_workflow", class_name: "PreservationWorkflow"
+
+  property :primary_language, predicate: "http://purl.org/dc/elements/1.1/language", multiple: false do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :primary_repository_ID, predicate: "http://purl.org/dc/terms/identifier", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :publisher, predicate: "http://purl.org/dc/elements/1.1/publisher", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :publisher_version, predicate: "http://metadata.emory.edu/vocab/cor-terms#publicationStage", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :re_use_license, predicate: "http://schema.org/license", multiple: false
+
+  property :related_datasets, predicate: "http://purl.org/dc/terms/relation#dataset"
+
+  property :related_material, predicate: "http://purl.org/dc/elements/1.1/relation" do |index|
+    index.as :stored_searchable
+  end
+
+  property :related_publications, predicate: "http://purl.org/dc/terms/relation#publication"
+
+  property :rights_documentation, predicate: "http://metadata.emory.edu/vocab/cor-terms#rightsDocumentationURI", multiple: false
+
+  property :rights_holder, predicate: "http://purl.org/dc/terms/rightsHolder"
+
+  property :rights_statement, predicate: "http://www.europeana.eu/schemas/edm/rights" do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :rights_statement_text, predicate: "http://purl.org/dc/elements/1.1/rights" do |index|
+    index.as :stored_searchable
+  end
+
+  property :scheduled_rights_review, predicate: "http://metadata.emory.edu/vocab/cor-terms#scheduledRightsReview", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :scheduled_rights_review_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#scheduledRightsReviewNote", multiple: false do |index|
     index.as :stored_searchable
   end
 
@@ -231,23 +219,34 @@ class CurateGenericWork < ActiveFedora::Base
   property :sensitive_material_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#sensitiveMaterialNote", multiple: false do |index|
     index.as :stored_searchable
   end
+
+  property :series_title, predicate: "http://id.loc.gov/ontologies/bibframe/seriesStatement", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :sponsor, predicate: "http://id.loc.gov/vocabulary/relators/spn", multiple: false
+
   property :staff_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#staffNote" do |index|
     index.as :stored_searchable
   end
 
-  property :date_digitized, predicate: "http://metadata.emory.edu/vocab/cor-terms#dateDigitized", multiple: false do |index|
+  property :subject_geo, predicate: "http://purl.org/dc/elements/1.1/coverage" do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :subject_names, predicate: "http://purl.org/dc/elements/1.1/subject#names" do |index|
+    index.as :stored_searchable, :facetable
+  end
+
+  property :subject_time_periods, predicate: "http://schema.org/temporalCoverage" do |index|
     index.as :stored_searchable
   end
 
-  property :transfer_engineer, predicate: "http://metadata.emory.edu/vocab/cor-terms#fileTransferEngineer", multiple: false do |index|
-    index.as :stored_searchable
+  property :subject_topics, predicate: "http://purl.org/dc/elements/1.1/subject" do |index|
+    index.as :stored_searchable, :facetable
   end
 
-  property :legacy_identifier, predicate: "http://id.loc.gov/vocabulary/identifiers/local#legacy" do |index|
-    index.as :stored_searchable
-  end
-
-  property :legacy_ark, predicate: "http://id.loc.gov/vocabulary/identifiers/local#ark" do |index|
+  property :sublocation, predicate: "http://metadata.emory.edu/vocab/cor-terms#physicalSublocation", multiple: false do |index|
     index.as :stored_searchable
   end
 
@@ -255,19 +254,23 @@ class CurateGenericWork < ActiveFedora::Base
     index.as :stored_searchable
   end
 
-  property :primary_repository_ID, predicate: "http://purl.org/dc/terms/identifier", multiple: false do |index|
+  property :table_of_contents, predicate: "http://purl.org/dc/terms/tableOfContents", multiple: false do |index|
     index.as :stored_searchable
   end
 
-  property :re_use_license, predicate: "http://schema.org/license", multiple: false
-
-  property :publisher, predicate: "http://purl.org/dc/elements/1.1/publisher", multiple: false do |index|
+  property :technical_note, predicate: "http://metadata.emory.edu/vocab/cor-terms#technicalNote", multiple: false do |index|
     index.as :stored_searchable
   end
 
-  property :date_created, predicate: "http://purl.org/dc/terms/created", multiple: false do |index|
-    index.as :stored_searchable, :facetable
+  property :transfer_engineer, predicate: "http://metadata.emory.edu/vocab/cor-terms#fileTransferEngineer", multiple: false do |index|
+    index.as :stored_searchable
   end
+
+  property :uniform_title, predicate: "http://purl.org/dc/elements/1.1/title", multiple: false do |index|
+    index.as :stored_searchable
+  end
+
+  property :volume, predicate: "http://purl.org/ontology/bibo/volume", multiple: false
 
   # accepts_nested_attributes_for can not be called until all
   # the properties are declared because it calls resource_class,
