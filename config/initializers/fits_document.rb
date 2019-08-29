629d682a03ec667a8b2a5cb3c5d502529115fe2f
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2019-08-29 10:40:09 -0400

    Adds additional file metadata properties (#518)
    Adds additional properties to fits_document for metadata extraction
    Copies and appends to hydra-works' characterization_service_spec

---
+++ config/initializers/fits_document.rb
@@ -0,0 +1,28 @@
+# Opens FitsDocument class from Hydra::Works::Characterization
+# and adds fits mapping for extra technical metadata
+Hydra::Works::Characterization::FitsDocument.class_eval do
+  PROXIED_TERMS = Object.const_get 'Hydra::Works::Characterization::FitsDocument::PROXIED_TERMS'
+  NEW_PROXIED_TERMS = PROXIED_TERMS.dup + %i[file_path creating_os creating_application_name puid].freeze
+
+  def self.terminology
+    struct = Struct.new(:proxied_term).new
+    terminology = Struct.new(:terms)
+    terminology.new(NEW_PROXIED_TERMS.map { |t| [t, struct] }.to_h)
+  end
+
+  def file_path
+    ng_xml.css("fits > fileinfo > filepath").map(&:text)
+  end
+
+  def creating_os
+    ng_xml.css("fits > fileinfo > creatingos").map(&:text)
+  end
+
+  def creating_application_name
+    ng_xml.css("fits > fileinfo > creatingApplicationName").map(&:text)
+  end
+
+  def puid
+    ng_xml.css("fits > identification > identity > externalIdentifier[type='puid']").map(&:text)
+  end
+end
