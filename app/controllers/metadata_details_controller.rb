6b64ee4c879a270e64c307a3bdf24f71d078e85b
Mark Bussey <mark@curationexperts.com>
2019-09-02 21:17:56 -0500

    Handle JSON format requests for metadata_details
    Adds support for requests to /importer_documentation/guide.json.
    Redirects any unrecognized request formats to .html

--- app/controllers/metadata_details_controller.rb
+++ app/controllers/metadata_details_controller.rb
@@ -2,6 +2,11 @@ class MetadataDetailsController < ApplicationController
   def show
     @details = ::MetadataDetails.instance.details(work_attributes:
                                                 CurateGenericWorkAttributes.instance)
+    respond_to do |format|
+      format.html
+      format.json { render json: @details.to_json }
+      format.any { redirect_to action: :show }
+    end
   end
 
   def profile
