031d7680c842c8dd7937f676fbd4df776c5044fe
Alex Zotov <alexandr.zotov@emory.edu>
2019-09-06 10:19:04 -0400

    implements preservation workflow model

--- app/actors/hyrax/actors/curate_generic_work_actor.rb
+++ app/actors/hyrax/actors/curate_generic_work_actor.rb
@@ -3,10 +3,16 @@
 module Hyrax
   module Actors
     class CurateGenericWorkActor < Hyrax::Actors::BaseActor
+      KNOWN_NESTED_ATTRIBUTES = [:preservation_workflow_attributes].freeze
       def apply_save_data_to_curation_concern(env)
-        # Insert break here to see what metadata is about to be saved on the object
-        # byebug
         super
+        # ActiveFedora fails to propgate changes to nested attributes to
+        # `#resource` when indexed nested attributes are used. We force the
+        # issue here to work around for form edits.
+        (env.attributes.keys && KNOWN_NESTED_ATTRIBUTES).each do |attribute_key|
+          attribute = attribute_key.to_s.gsub('_attributes', '').to_sym
+          env.curation_concern.send(attribute).each { |member| member.try(:persist!) }
+        end
       end
     end
   end
