8dc5c3be140ebfea6f4208304f13db4eab8b765d
Jamie Little <jamie@jamielittle.org>
2019-09-09 12:20:03 -0500

    Give a warning when file is missing
    This adds a method to the validator to check that
    the file exists. If the file does not exist it provides
    a warning in the UI when you upload the CSV.
    
    Since this is a warning and not an error, this changes
    the importer so that it will not raise errors if you
    continue the import with a CSV file that does not have
    files.
    
    Connected to curationexperts/dlp-curate#39

---
+++ app/uploaders/zizia/warning_appender.rb
@@ -0,0 +1,14 @@
+module Zizia
+  class WarningAppender
+    attr_accessor :row, :warnings
+
+    def initialize(row:, warnings:)
+      @row = row
+      @warnings = warnings
+    end
+
+    def warning; end
+
+    def condition; end
+  end
+end
