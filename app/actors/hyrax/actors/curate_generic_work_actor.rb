4c48baccd776b54199502680792934a8d521047a
Alex Zotov <alexandr.zotov@emory.edu>
2019-09-09 13:29:02 -0400

    Update curate_generic_work_actor.rb

--- app/actors/hyrax/actors/curate_generic_work_actor.rb
+++ app/actors/hyrax/actors/curate_generic_work_actor.rb
@@ -4,6 +4,10 @@ module Hyrax
   module Actors
     class CurateGenericWorkActor < Hyrax::Actors::BaseActor
       KNOWN_NESTED_ATTRIBUTES = [:preservation_workflow_attributes].freeze
+      # Some CSV rows have blank metadata and are only used to attach a file.
+      # Those rows should come through with env.attributes["skip_metadata"] = true
+      # 1. Remove that value from the attributes, since it will cause an error if you try to save it to the object
+      # 2. If skip_metadata == true, do not apply the metadata to the object, but do all other expected update behavior
       def apply_save_data_to_curation_concern(env)
         super
         # ActiveFedora fails to propgate changes to nested attributes to
@@ -13,12 +17,7 @@ module Hyrax
           attribute = attribute_key.to_s.gsub('_attributes', '').to_sym
           env.curation_concern.send(attribute).each { |member| member.try(:persist!) }
         end
-      end
-      # Some CSV rows have blank metadata and are only used to attach a file.
-      # Those rows should come through with env.attributes["skip_metadata"] = true
-      # 1. Remove that value from the attributes, since it will cause an error if you try to save it to the object
-      # 2. If skip_metadata == true, do not apply the metadata to the object, but do all other expected update behavior
-      def apply_save_data_to_curation_concern(env)
+
         skip_metadata = env.attributes["skip_metadata"]
         env.attributes.delete(:skip_metadata)
         env.curation_concern.attributes = clean_attributes(env.attributes) unless skip_metadata
