de7d0bf52e0a66f172dcb1fcd2a02dca38f3c2a7
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2019-09-20 10:43:05 -0400

    Adds user-groups to langmuir collection (#594)

--- spec/importers/collection_permission_ensurer_spec.rb
+++ spec/importers/collection_permission_ensurer_spec.rb
@@ -3,11 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe CollectionPermissionEnsurer, :clean do
-  let(:collection) { Collection.new }
-  let(:ensurer) { described_class.new(collection: collection, access_permissions: ['manage', 'deposit']) }
+  let(:collection) { FactoryBot.create(:collection_lw) }
+  let(:ensurer) { described_class.new(collection: collection, access_permissions: { 'manage' => ["admin", "rose_manager"], 'deposit' => ["admin", "rose_depositor"], 'view' => ["rose_viewer"] }) }
 
   it 'sets the access permssions specified in the permissons array' do
+    allow(Role).to receive(:exists?).and_return(true)
     ensurer
     expect(Hyrax::PermissionTemplateAccess.where(agent_id: 'admin').count).to eq(2)
+    expect(Hyrax::PermissionTemplateAccess.where(agent_id: 'rose_manager').count).to eq(1)
+    expect(Hyrax::PermissionTemplateAccess.where(agent_id: 'rose_depositor').count).to eq(1)
+    expect(collection.edit_groups).to eq(["admin", "rose_manager"])
+    expect(collection.read_groups).to eq(["public", "rose_viewer"])
   end
 end
