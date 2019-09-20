b2aae8fc39024a40075b8569a964e2e7dbf6a279
Jamie Little <jamie@jamielittle.org>
2019-09-20 09:54:26 -0500

    Remove visibility constant when loading Rails so it can be re-defined
    This constant needs to be removed from `hydra-access-controls` and
    replaced with out custom list. In prod, it is not enough to re-define
    it in an initializer. It needs to be removed explicility.

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,16 @@ require_relative 'application'
 Rails.application.initialize!
 Rails.logger.datetime_format = "%Y-%m-%d %H:%M:%S"
 Rails.logger.level = Logger::INFO
+
+# Remove the existing visibility label constant
+Hyrax::PermissionBadge.send(:remove_const, :VISIBILITY_LABEL_CLASS)
+Hyrax::PermissionBadge::VISIBILITY_LABEL_CLASS = {
+  authenticated: "label-info",
+  embargo: "label-warning",
+  lease: "label-warning",
+  open: "label-success",
+  restricted: "label-danger",
+  low_res: "label-success",
+  emory_low: "label-info",
+  rose_high: "label-warning"
+}.freeze
