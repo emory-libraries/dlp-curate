29d6af0ba4a2b1fad0032e99995deb714ce7f81e
SolomonHD <solomon.hilliard@emory.edu>
2019-10-02 08:04:19 -0700

    Rename bareboes to min, make a version for each environment

---
+++ config/deploy/min_production.rb
@@ -0,0 +1,9 @@
+set :stage, :PROD
+ec2_role [:web, :app, :db, :redhatapp],
+         user: 'deploy',
+         ssh_options: {
+           keys: ENV['SSH_EC2_KEY_FILE'],
+           forward_agent: true,
+           verify_host_key: :never
+         }
+# server 'PRIVATE_IP_Address', user: 'deploy', roles: [:web, :app, :db, :redhatapp]
