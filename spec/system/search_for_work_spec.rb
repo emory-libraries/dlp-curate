8db84efc1cad1153c95873edc19c59bc883cf11a
Will Ulman <46227821+wulman16@users.noreply.github.com>
2019-10-04 10:37:52 -0400

    Allow works to be searched by ID (#642)
    * Allow works to be searched by ID
    
    * Refactor search spec

---
+++ spec/system/search_for_work_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+require 'rails_helper'
+include Warden::Test::Helpers
+
+RSpec.describe 'Search', type: :system do
+  let(:admin_user) { FactoryBot.build(:admin) }
+  let(:work) { FactoryBot.build(:work_with_full_metadata, :public) }
+  let(:title) { work.title.first }
+
+  before do
+    login_as admin_user
+    work.save!
+  end
+
+  it 'finds a work by ID' do
+    visit root_path
+    fill_in "search-field-header", with: work.id
+    click_button "search-submit-header"
+    expect(page).to have_content "1 entry found"
+    expect(page).to have_content title
+  end
+end
