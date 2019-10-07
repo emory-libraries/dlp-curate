4e7018ba2c5365501a3fd9df0d7ccce105a4eddc
Mark Bussey <mark@curationexperts.com>
2019-10-07 01:58:23 -0500

    Add Alma mappings to Yellowback preprocessor

--- app/importers/yellowback_preprocessor.rb
+++ app/importers/yellowback_preprocessor.rb
@@ -5,7 +5,7 @@ require 'csv'
 # Utility service and methods that merge metadata from a CSV Pull List and MARCXml records
 # into a format suitable for ingest by the curate CSV importer
 
-class YellowbackPreprocessor
+class YellowbackPreprocessor # rubocop:disable Metrics/ClassLength
   attr_accessor :processed_csv
 
   ##
@@ -21,44 +21,240 @@ class YellowbackPreprocessor
     @processed_csv = File.join(directory, filename + "-merged.csv")
   end
 
-  def merge # rubocop:disable Metrics/MethodLength
+  HEADER_FIELDS = [
+    # Context fields to help humans compare this file to sources
+    'pl_row',
+    'work_id',
+    'CSV title',
+    # Fields extracted from the csv pull list
+    'administrative_unit',
+    'content_type',
+    'data_classifications',
+    'emory_ark',
+    'emory_rights_statements',
+    'holding_repository',
+    'institution',
+    'other_identifiers',
+    'rights_statement',
+    'system_of_record_ID',
+    # Fields extracted from Alma MARC records
+    'conference_name',
+    'contributor',
+    'copyright_date',
+    'creator',
+    'date_created',
+    'date_digitized',
+    'date_issued',
+    'edition',
+    'extent',
+    'genre',
+    'local_call_number',
+    'place_of_production',
+    'primary_language',
+    'publisher',
+    'series_title',
+    'subject_geo',
+    'subject_names',
+    'subject_topics',
+    'table_of_contents',
+    'title',
+    'uniform_title'
+  ].freeze
+
+  def merge
     merge_csv = CSV.open(@processed_csv, 'w+', headers: true, write_headers: true)
-    headers = [
-      # Context fields to help humans compare this file to sources
-      'pl_row',
-      'work_id',
-      'title',
-      # Fields extracted from the csv pull list
-      'administrative_unit',
-      'content_type',
-      'data_classifications',
-      'emory_ark',
-      'emory_rights_statements',
-      'holding_repository',
-      'institution',
-      'other_identifiers',
-      'rights_statement',
-      'system_of_record_ID'
-    ]
-    merge_csv << headers
+    merge_csv << HEADER_FIELDS
     @pull_list.each.with_index do |row, csv_index|
-      new_row = [
-        csv_index + 2,    # pl_row  (original row number from pull list)
-        row['emory_ark'], # work_id
-        row['CSV Title'], # title
-        row['administrative_unit'], # administrative_unit
-        row['content_type'], # content_type
-        row['data_classifications'], # data_classification
-        row['emory_ark'], # emory_ark
-        row['emory_rights_statements'], # emory_rights_statement
-        row['holding_repository'], # holding_repository
-        row['institution'], # institution
-        row['other_identifiers'], # other_identifiers
-        row['rights_statement'], # rights_statement
-        row['system_of_record_ID'], # system_of_record_ID
-      ]
+      mmsid = row['ALMA MMSID']
+      record = @marc_records.xpath("//record/controlfield[@tag='001'][text()='#{mmsid}']/ancestor::record").first
+      new_row = context_fields(csv_index, row)
+      new_row += pull_list_mappings(row)
+      new_row += alma_mappings(record, row)
       merge_csv << new_row
     end
     merge_csv.close
   end
+
+  private
+
+    def context_fields(csv_index, row)
+      [
+        csv_index + 2, # pl_row  (original row number from pull list)
+        row['emory_ark'], # work_id
+        row['CSV Title'] # title
+      ]
+    end
+
+    def pull_list_mappings(row)
+      [
+        row['administrative_unit'],
+        row['content_type'],
+        row['data_classifications'],
+        row['emory_ark'],
+        row['emory_rights_statements'],
+        row['holding_repository'],
+        row['institution'],
+        row['other_identifiers'],
+        row['rights_statement'],
+        row['system_of_record_ID']
+      ]
+    end
+
+    def alma_mappings(record, row) # rubocop:disable Metrics/MethodLength
+      [
+        conference_name(record),
+        contributor(record),
+        copyright_date(record),
+        creator(record),
+        date_created(record),
+        date_digitized(record),
+        date_issued(record),
+        edition(record),
+        extent(record),
+        genre(record),
+        local_call_number(record),
+        place_of_production(record),
+        primary_language(record),
+        publisher(record),
+        series_title(record),
+        subject_geo(record),
+        subject_names(record),
+        subject_topics(record),
+        table_of_contents(record),
+        title(record, row),
+        uniform_title(record)
+      ]
+    end
+
+    def conference_name(marc_record)
+      extract_datafields(marc_record, '611')
+    end
+
+    def contributor(marc_record)
+      [extract_datafields(marc_record, '700'),
+       extract_datafields(marc_record, '710')].join('|')
+    end
+
+    def copyright_date(marc_record)
+      date_created(marc_record)
+    end
+
+    def creator(marc_record)
+      extract_datafields(marc_record, '100')
+    end
+
+    def date_created(marc_record)
+      node_260_date = marc_record.xpath("./datafield[@tag='260']/subfield[@code='c']").text
+      node_264_date = marc_record.xpath("./datafield[@tag='264']/subfield[@code='c']").text
+      publication_date = clean_marc_date(node_260_date)
+      production_date = clean_marc_date(node_264_date)
+      if production_date
+        production_date
+      elsif publication_date
+        publication_date
+      else
+        'XXXX'
+      end
+    end
+
+    def date_digitized(marc_record)
+      extract_datafields(marc_record, '583')
+    end
+
+    def date_issued(marc_record)
+      date_created(marc_record)
+    end
+
+    def edition(marc_record)
+      extract_datafields(marc_record, '250')
+    end
+
+    def extent(marc_record)
+      extract_datafields(marc_record, '300')
+    end
+
+    def genre(marc_record)
+      extract_datafields(marc_record, '655')
+    end
+
+    def local_call_number(marc_record)
+      extract_datafields(marc_record, '090')
+    end
+
+    def place_of_production(marc_record)
+      place_of_production_nodes = marc_record.xpath("./datafield[@tag='264']")
+      place_of_production_nodes.xpath("./subfield[@code='a']").map { |s| s.text.scan(/(.+)\s[\:\;]$/) }.flatten.join('|')
+    end
+
+    def primary_language(marc_record)
+      marc_record.xpath("./controlfield[@tag='008']").text.strip
+    end
+
+    def publisher(marc_record)
+      publisher_nodes = marc_record.xpath("./datafield[@tag='264']")
+      publisher_nodes.xpath("./subfield[@code='b']/text()").map { |s| s.text.scan(/(.+)[.,]$/) }.flatten.join('|')
+    end
+
+    def series_title(marc_record)
+      extract_datafields(marc_record, '830')
+    end
+
+    def subject_geo(marc_record)
+      subject_geo_nodes = marc_record.xpath("./datafield[@tag='651']")
+      subject_geo_nodes.map { |n| n.xpath("./subfield/text()").map(&:text).join("--") }.join('|') #  join subfields with '--' and multiple datafields with '|'
+    end
+
+    def subject_names(marc_record)
+      extract_datafields(marc_record, '600')
+    end
+
+    def subject_topics(marc_record)
+      subject_topics_nodes = marc_record.xpath("./datafield[@tag='650']")
+      subject_topics_nodes.map { |n| n.xpath("./subfield/text()").map(&:text).join("--") }.join('|') #  join subfields with '--' and multiple datafileds with '|'
+    end
+
+    def table_of_contents(marc_record)
+      extract_datafields(marc_record, '505')
+    end
+
+    def title(marc_record, csv_row)
+      title_nodes = marc_record.xpath("./datafield[@tag='245']")
+      title_segments = title_nodes.xpath("./subfield[@code='a' or @code='b']/text()")
+      alma_title = title_segments.map(&:text).join(" ").chomp("/").strip # join title segments with a space, then remove any trailing / or whitespace
+      enumeration = csv_row["Enumeration"]
+      if enumeration
+        alma_title + " [#{enumeration}]"
+      else
+        alma_title
+      end
+    end
+
+    def uniform_title(marc_record)
+      extract_datafields(marc_record, '240')
+    end
+
+    def extract_datafields(marc_record, field_no)
+      selected_nodes = marc_record.xpath("./datafield[@tag='#{field_no}']")
+      concatenate_marc_datafields(selected_nodes)
+    end
+
+    def concatenate_marc_datafields(nodes = [])
+      if nodes.count > 1
+        nodes.map { |n| concatenate_marc_subfields(n) }.join('|')
+      elsif nodes.count == 1
+        concatenate_marc_subfields(nodes)
+      end
+    end
+
+    def concatenate_marc_subfields(datafield)
+      datafield.xpath('./subfield').map { |s| s.text.strip }.join(' ')
+    end
+
+    # Remove MARC puctuation from a date and return a 4-digit year or nil
+    # e.g. "[1891.]" --> "1891"
+    #      "19XX." --> nil
+    #      "19th Cent."  --> nil
+    def clean_marc_date(date_string)
+      date_string.scan(/^\[?(\d{4})\D*/).flatten.first
+    end
 end
--- spec/importers/yellowback_preprocessor_spec.rb
+++ spec/importers/yellowback_preprocessor_spec.rb
@@ -26,11 +26,11 @@ RSpec.describe YellowbackPreprocessor do
 
   # this just repeats the csv data, but shows the testing pattern and titles used in subsequent tests
   it 'processes the expected works' do
-    expect(import_rows[0]['title']).to eq('Edge cases') # Edge cases
-    expect(import_rows[1]['title']).to eq('Shakespeare\'s comedy of The merchant of Venice /') # Shakespeare's comedy of The merchant of Venice
+    expect(import_rows[0]['title']).to eq('Edge cases : miscellaneous & sundry') # Edge cases
+    expect(import_rows[1]['title']).to eq('Shakespeare\'s comedy of The merchant of Venice') # Shakespeare's comedy of The merchant of Venice
     expect(import_rows[2]['title']).to eq('Choice bits from Mark Twain.') # Choice bits from Mark Twain
-    expect(import_rows[3]['title']).to eq('The common moths of England /') # The common moths of England
-    expect(import_rows[4]['title']).to eq('The common moths of England /') # The common moths of England
+    expect(import_rows[3]['title']).to eq('The common moths of England [Copy 2]') # The common moths of England
+    expect(import_rows[4]['title']).to eq('The common moths of England [Copy 3]') # The common moths of England
   end
 
   # make sure we account for the header row
@@ -48,8 +48,8 @@ RSpec.describe YellowbackPreprocessor do
     expect(import_rows[4]['work_id']).to eq('7st50') # copy 2
   end
 
-  # Pull List tests:
-  # fields that should be extracted and mapped from the Pull List CSV
+  # Pull List tests ############################################
+  # fields that are extracted and mapped from the Pull List CSV
   it 'extracts administrative_unit values from the pull list' do
     expect(import_rows[1]['administrative_unit']).to eq('Stuart A. Rose Manuscript, Archives, and Rare Book Library') # Shakespeare's comedy of The merchant of Venice
   end
@@ -90,5 +90,121 @@ RSpec.describe YellowbackPreprocessor do
 
   it 'extracts the system_of_record_ID from the pull list' do
     expect(import_rows[1]['system_of_record_ID']).to eq('alma:990024722450302486') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  # Alma tests ###############################################
+  # Fields that are extracted and mapped from MARCXml records
+  it 'extracts conference_name from Alma' do
+    expect(import_rows[0]['conference_name']).to eq('International Exhibition (1862 : London, England)') # Edge cases
+  end
+
+  it 'extracts copyright_date from Alma' do
+    expect(import_rows[1]['copyright_date']).to eq('1898') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'extracts contributor information from Alma' do
+    expect(import_rows[1]['contributor']).to eq('Gollancz, Israel, 1864-1930.|' + # Shakespeare's comedy of The merchant of Venice
+                                                'Hughes, Ted, 1930-1998, former owner. GEU|' +
+                                                'Ted Hughes Library (Emory University. General Libraries) GEU')
+  end
+
+  it 'extracts creator information from Alma' do
+    expect(import_rows[1]['creator']).to eq('Shakespeare, William, 1564-1616.') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'extracts date_created from Alma' do
+    expect(import_rows[1]['date_created']).to eq('1898') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'sets date_created to XXXX when no date is specified' do
+    expect(import_rows[0]['date_created']).to eq('XXXX') # Edge Cases
+  end
+
+  it 'extracts date_digitized from Alma' do
+    expect(import_rows[1]['date_digitized']).to eq('2009') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'extracts date_issued from Alma' do
+    expect(import_rows[1]['date_issued']).to eq('1898') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'extracts edition from Alma' do
+    expect(import_rows[1]['edition']).to eq('5th edition.') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'extracts extent from Alma' do
+    expect(import_rows[3]['extent']).to eq('154 pages, 12 leaves of plates : illustrations ; 17 cm.') # The common moths of England
+  end
+
+  it 'extracts genre from Alma' do
+    expect(import_rows[2]['genre']).to eq('Yellowbacks.') # Choice bits from Mark Twain
+  end
+
+  it 'leaves genre blank if no value exists Alma' do
+    expect(import_rows[1]['genre']).to be_nil # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'extracts local_call_number from Alma' do
+    expect(import_rows[4]['local_call_number']).to eq('QL555.G7 W85') # The common moths of England
+  end
+
+  it 'extracts primary_language from Alma' do
+    expect(import_rows[1]['primary_language']).to eq('eng') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'extracts publisher from Alma' do
+    expect(import_rows[1]['publisher']).to eq('J.M. Dent') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'handles multiple publisher entries' do
+    expect(import_rows[0]['publisher']).to eq('Chatto & Windus, Piccadilly|Printed by William Clowes and Sons, Limited') # Edge Cases
+  end
+
+  it 'extracts place_of_production from Alma' do
+    expect(import_rows[1]['place_of_production']).to eq('London') # Shakespeare's comedy of The merchant of Venice
+  end
+
+  it 'handles multiple place_of_production entries' do
+    expect(import_rows[0]['place_of_production']).to eq('London|London and Beccles') # Edge Cases
+  end
+
+  it 'extracts table_of_contents from Alma' do
+    expect(import_rows[2]['table_of_contents']).to match(/^Autobiography -- Memoranda -- The facts in the case .../) # Choice bits from Mark Twain
+  end
+
+  it 'extracts titles from Alma' do
+    expect(import_rows[1]['title']).to eq('Shakespeare\'s comedy of The merchant of Venice')
+  end
+
+  it 'extracts only subfields (a) and (b) from titles' do
+    expect(import_rows[0]['title']).to eq('Edge cases : miscellaneous & sundry')
+  end
+
+  it 'handles character encodings correctly' do
+    expect(import_rows[0]['uniform_title']).to eq('Belle geôlière.') # Edge cases
+  end
+
+  it 'extracts uniform_title from Alma' do
+    expect(import_rows[2]['uniform_title']).to eq('Works.') # Choice bits from Mark Twain.
+  end
+
+  it 'extracts series_title from Alma' do
+    expect(import_rows[0]['series_title']).to eq('Routledge\'s railway library.|' + # Edge cases
+                                                   'Jarrolds\' railway library.')
+  end
+
+  it 'extracts subject_geo from Alma' do
+    expect(import_rows[0]['subject_geo']).to eq('London (England)--Fiction.') # Edge cases
+  end
+
+  it 'extracts subject_names from Alma' do
+    expect(import_rows[0]['subject_names']).to eq('Mary, Queen of Scots, 1542-1587|' + # Edge cases
+                                                   'Bothwell, James Hepburn, Earl of, 1536?-1578')
+  end
+
+  it 'extracts subject_topics from Alma' do
+    expect(import_rows[0]['subject_topics']).to eq('Aristocracy (Social class)--Fiction.|' + # Edge cases
+                                                    'Upper class families--Fiction.|' +
+                                                    'Dandies--Fiction.')
   end
 end
