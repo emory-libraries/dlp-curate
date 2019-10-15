e6e643c3033548f4ff322006bb3bf4b25918ba21
devanshu-m <devanshu_m@gwmail.gwu.edu>
2019-10-15 12:14:00 -0400

    Adds preservation_event for fileset creation and saves the event on fileset in fedora

--- spec/actors/hyrax/actors/file_set_actor_spec.rb
+++ spec/actors/hyrax/actors/file_set_actor_spec.rb
@@ -54,5 +54,17 @@ RSpec.describe Hyrax::Actors::FileSetActor do
         expect(file_set.label).to eql(label)
       end
     end
+
+    context 'when a fileset is saved' do
+      before do
+        actor.create_content(file)
+      end
+
+      it 'adds a new preservation_event for fileset creation' do
+        expect(file_set.preservation_event.first.event_type).to eq ['Replication (FileSet created)']
+        expect(file_set.preservation_event.first.outcome).to eq ['Success']
+        expect(file_set.preservation_event.count).to eq 1
+      end
+    end
   end
 end
