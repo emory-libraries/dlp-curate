31c572fba1d31478af20d966367cac01a2217193
Jamie Little <jamie@jamielittle.org>
2019-10-29 13:42:31 -0500

    Update Zizia to 5.2.0 and save deduplication_key during import
    This saves the `deduplication_key` on
    `PreIngestWork`s when running an import.
    
    Connected to https://github.com/curationexperts/in-house/issues/442

--- db/schema.rb
+++ db/schema.rb
@@ -587,6 +587,7 @@ ActiveRecord::Schema.define(version: 201901241536542) do
     t.integer "failure_count"
     t.string "deduplication_field"
     t.string "update_actor_stack"
+    t.string "status"
     t.index ["csv_import_id"], name: "index_zizia_csv_import_details_on_csv_import_id"
   end
 
@@ -616,7 +617,9 @@ ActiveRecord::Schema.define(version: 201901241536542) do
     t.integer "csv_import_detail_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "deduplication_key"
     t.index ["csv_import_detail_id"], name: "index_zizia_pre_ingest_works_on_csv_import_detail_id"
+    t.index ["deduplication_key"], name: "index_zizia_pre_ingest_works_on_deduplication_key"
   end
 
 end
