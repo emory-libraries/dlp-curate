8b9a067101867dc6415fd933ab7abb42b244a8d4
Bess Sadler <bess@curationexperts.com>
2019-11-14 14:48:40 -0500

    Update visibility badges to match Emory expectations
    Update visibility badges to match specifications from Curate user group

---
+++ config/prepends/custom_permission_badge.rb
@@ -0,0 +1,11 @@
+module CustomPermissionBadge
+  private
+
+    # We are overriding this method from Hyrax because we do not want to display the
+    # institution name in the Authenticated permission badge
+    def text
+      I18n.t("hyrax.visibility.#{@visibility}.text")
+    rescue
+      @visibility.to_s
+    end
+end
