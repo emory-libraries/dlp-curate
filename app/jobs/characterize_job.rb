9dc27b9dbd334601d0ae19f96636b46924dfbaa6
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2019-11-20 13:57:34 -0500

    Edits event_details for characterization event (#777)
    * This commit adds the relation and filename to the event_details when
    creating a characterization event

--- app/jobs/characterize_job.rb
+++ app/jobs/characterize_job.rb
@@ -13,15 +13,17 @@ class CharacterizeJob < Hyrax::ApplicationJob
   # @param [String, NilClass] filepath the cached file within the Hyrax.config.working_path
   def perform(file_set, file_id, filepath = nil)
     event_start = DateTime.current
-    raise "#{file_set.class.characterization_proxy} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
+    relation = file_set.class.characterization_proxy
+    file = file_set.characterization_proxy
+    raise "#{relation} was not found for FileSet #{file_set.id}" unless file_set.characterization_proxy?
     filepath = Hyrax::WorkingDirectory.find_or_retrieve(file_id, file_set.id) unless filepath && File.exist?(filepath)
-    Hydra::Works::CharacterizationService.run(file_set.characterization_proxy, filepath)
+    Hydra::Works::CharacterizationService.run(file, filepath)
     event = { 'type' => 'Characterization', 'start' => event_start, 'outcome' => 'Success',
-              'details' => 'Technical metadata extracted from file, format identified, and file validated',
+              'details' => "#{relation}: #{file.file_name.first} - Technical metadata extracted from file, format identified, and file validated",
               'software_version' => 'FITS v1.5.0', 'user' => file_set.depositor }
     create_preservation_event(file_set, event)
-    Rails.logger.debug "Ran characterization on #{file_set.characterization_proxy.id} (#{file_set.characterization_proxy.mime_type})"
-    file_set.characterization_proxy.save!
+    Rails.logger.debug "Ran characterization on #{file.id} (#{file.mime_type})"
+    file.save!
     file_set.update_index
     file_set.parent&.in_collections&.each(&:update_index)
     CreateDerivativesJob.perform_later(file_set, file_id, filepath)
