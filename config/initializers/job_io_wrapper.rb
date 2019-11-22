5dd5bc66119d73813b4f95571ee32151b44d45d0
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2019-11-22 10:37:26 -0500

    Revises where file_replication/submission event happens (#781)
    * This commit removes the replication event from the file_set_actor
    since we are no longer logging when the file_set saves.
    * We are now creating an event on file ingest in the job_io_wrapper
    model. We look for what is returned from the file_actor.ingest_file
    method. If `false` is returned and the file is not attached to the file_set,
    we create a failure event, else a success event.

---
+++ config/initializers/job_io_wrapper.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+# [Hyrax-model-overwrite]
+# ingest_file method in the JobIoWrapper method is modified. We save the response
+# from the `file_actor.ingest_file` method call. If false is returned from L#16 in
+# `config/intializers/file_actor.rb` then a failure event is created, else success
+# event.
+
+JobIoWrapper.class_eval do
+  include PreservationEvents
+
+  def ingest_file
+    event_start = DateTime.current
+    file_name = file.path.to_s.split("/").last
+    result = file_actor.ingest_file(self)
+    if result == false
+      outcome = 'Failure'
+      details = "File not replicated to cross-region S3 storage: #{file_name}"
+    else
+      outcome = 'Success'
+      details = "File replicated to cross-region S3 storage: #{file_name}"
+    end
+    file_set_preservation_event(file_set, event_start, outcome, details)
+  end
+
+  private
+
+    # create preservation_event for fileset creation (method in PreservationEvents module)
+    def file_set_preservation_event(file_set, event_start, outcome, details)
+      event = { 'type' => 'File submission', 'start' => event_start, 'outcome' => outcome, 'details' => details,
+                'software_version' => 'Fedora v4.7.5', 'user' => user.uid }
+      create_preservation_event(file_set, event)
+    end
+end
