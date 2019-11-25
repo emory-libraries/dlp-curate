46e042d153f551247c90c25a409b5e6c0335b18c
Solomon Hilliard <solomon.hilliard@emory.edu>
2019-11-25 18:36:34 -0500

    Up output level for logging

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -2,7 +2,8 @@ Hydra::Works.default_system_virus_scanner = ::ClambyScanner
 
 if Rails.env.production?
   Clamby.configure(
-    check:  false, # only used for development environment
+    check: false, # only used for development environment
+    output_level: 'high',
     daemonize: true,
     error_clamscan_missing: true,
     error_file_missing: true,
