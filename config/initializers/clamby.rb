b1e07a82e2f5402582024e3d9dbd5f97023c4c68
Solomon Hilliard <solomon.hilliard@emory.edu>
2019-11-25 23:36:39 -0500

    Remove hard coding config path

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -3,7 +3,6 @@ Hydra::Works.default_system_virus_scanner = ::ClambyScanner
 if Rails.env.production?
   Clamby.configure(
     check: false, # only used for development environment
-    config_file: '/etc/clamd.d/scan.conf',
     daemonize: true,
     error_clamscan_missing: true,
     error_file_missing: true,
