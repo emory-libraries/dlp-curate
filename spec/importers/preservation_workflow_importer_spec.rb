01cd01ba144afc2b7e378a9bf5197b8853a088c0
devanshu-m <devanshu_m@gwmail.gwu.edu>
2019-11-26 19:36:06 -0500

    Adds importer for preservation_workflow
    * This commit adds an importer for bulk-importing preservation_workflow
    metadata for works.
    * We create a rake task and call the `import` method in the importer
    class. We go through all rows in the CSV and do the import process
    only on rows of `type` work. We first find the work and check if an
    ingest or accession workflow already exists (this is for update purposes).
    If they do exist, we delete the existing respective workflow and
    create a new one, else we just create a new workflow normally.

---
+++ spec/importers/preservation_workflow_importer_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
+RSpec.describe PreservationWorkflowImporter, :clean do
+  let(:generic_work) { CurateGenericWork.where(deduplication_key: 'MSS1218_B071_I205').first }
+  let(:csv)          { fixture_path + '/preservation_workflows.csv' }
+  let(:csv2)         { fixture_path + '/preservation_workflows2.csv' }
+
+  describe "#import" do
+    before do
+      CurateGenericWork.create(title: ['Example title'], deduplication_key: 'MSS1218_B071_I205')
+      described_class.import(csv)
+    end
+
+    context "with new preservation_workflow for the work" do
+      it "creates new preservation_workflows" do
+        expect(generic_work.preservation_workflow.count).to eq 2
+        expect(generic_work.preservation_workflow.pluck(:workflow_type)).to include ['Ingest']
+        expect(generic_work.preservation_workflow.pluck(:workflow_type)).to include ['Accession']
+        expect(generic_work.preservation_workflow.select { |w| w.workflow_type == ['Accession'] }.first.workflow_rights_basis_reviewer).to eq ['Scholarly Communications Office']
+      end
+    end
+
+    context "with existing preservation_workflow for the work" do
+      before do
+        described_class.import(csv2)
+      end
+
+      it "updates preservation_workflow" do
+        expect(generic_work.preservation_workflow.count).to eq 2
+        expect(generic_work.preservation_workflow.select { |w| w.workflow_type == ['Accession'] }.first.workflow_rights_basis_reviewer).to eq ['LTDS']
+      end
+    end
+  end
+end
