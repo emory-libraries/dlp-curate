78c229a64784e81ead2ccf99ff42c9a21baa7f86
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2019-12-09 13:14:48 -0500

    adds fixity check button on file_set show page (#812)
    * Adds fixity check button on file_set show page
    
    * This commit adds a button for running fixity check on demand.
    * We modify the create method in the FixityChecksController since
    we no longer need to render json, but redirect to the file_set show
    page with the result from the fixity check.
    
    * Adds failure test for fixity check
    
    * This commit adds a scenario when fixity check fails for one of
    the files in a file_set.
    * Also, removes notice since results can return more than one value,
    meaning, there could be multiple `passed` attributes for results,
    some might be true, some false. Easier to just omit notice and
    have something generic. Users can look for result in file_set details.

---
+++ spec/controllers/hyrax/fixity_checks_controller_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+# [Hyrax-overwrite]
+# We have removed json_response tests from here since we are
+# no longer rendering json in our create method
+require 'rails_helper'
+
+RSpec.describe Hyrax::FixityChecksController do
+  routes { Hyrax::Engine.routes }
+  let(:user) { FactoryBot.create(:user) }
+  let(:file_set) { FactoryBot.create(:file_set, user: user) }
+  let(:binary) { File.open(fixture_path + '/world.png') }
+  let(:file) { Hydra::Derivatives::IoDecorator.new(binary, 'image/png', 'world.png') }
+
+  before { Hydra::Works::UploadFileToFileSet.call(file_set, file) }
+
+  context "when signed in" do
+    describe "POST create" do
+      before do
+        sign_in user
+        post :create, params: { file_set_id: file_set }, xhr: true
+      end
+
+      it "returns result and redirects to file_set page" do
+        expect(response).to be_success
+        expect(response.redirect_url).to include "/concern/file_sets/#{file_set.id}"
+      end
+    end
+  end
+
+  context "when not signed in" do
+    describe "POST create" do
+      it "returns json with the result" do
+        post :create, params: { file_set_id: file_set }, xhr: true
+        expect(response.code).to eq '401'
+      end
+    end
+  end
+end
