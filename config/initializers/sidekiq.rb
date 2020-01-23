764932fbfbea617dfd316d12e3b53d529bb57e01
Solomon Hilliard <shilli2@emory.edu>
2020-01-23 16:43:48 -0500

    Add sidekiq initializer for non localhost

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+# config/initializers/sidekiq.rb
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
+end
+  
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
+end
