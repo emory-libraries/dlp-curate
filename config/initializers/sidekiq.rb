9796f67883ceb39c7797989992ffcbcb43b8c3ed
Collin Brittle <cnbrittle@gmail.com>
2020-01-31 18:16:35 +0000

    Fixes Sidekiq Redis URL
    The config hash does not work with symbols

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 redis_config = Rails.application.config_for(:redis)
-redis_url = "redis://#{redis_config[:host]}:#{redis_config[:port]}/#{redis_config[:db]}"
+redis_url = "redis://#{redis_config['host']}:#{redis_config['port']}/#{redis_config['db']}"
 
 Sidekiq.configure_server do |config|
   config.redis = { url: redis_url }
