c6fc41802200f4b74b1091d439be8eb3df56f8fa
Collin Brittle <cnbrittle@gmail.com>
2020-01-31 00:17:20 +0000

    Refactors redis/sidekiq config
    DRYs up config, and simplifies setting that configuration.
    
    Moves Redis initializer to a standard name.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
+redis_config = Rails.application.config_for(:redis)
+redis_url = "redis://#{redis_config[:host]}:#{redis_config[:port]}/#{redis_config[:db]}"
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST'] || 'localhost'}:#{ENV['REDIS_PORT'] || 6379}/#{ENV['REDIS_DATABASE'] || 0}" }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://#{ENV['REDIS_HOST'] || 'localhost'}:#{ENV['REDIS_PORT'] || 6379}/#{ENV['REDIS_DATABASE'] || 0}" }
+  config.redis = { url: redis_url }
 end
