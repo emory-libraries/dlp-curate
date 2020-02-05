40f58655ed7443265b1e37266a3875bf0dbc038f
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2020-02-05 15:28:45 -0500

    Implements FileSetCleanUp as a background job (#949)
    Changes how the rake task runs the file_set cleanup stuff by moving it into a background job. This way we can run the rake task which will exit almost immediately after kicking off the FileSetCleanUpJob that runs as a background job in sidekiq.

---
+++ app/jobs/file_set_clean_up_job.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class FileSetCleanUpJob < Hyrax::ApplicationJob
+  def perform
+    CSV.open("config/emory/index_file_set_results.csv", "w") do |csv|
+      FileSet.all.each do |file_set|
+        if file_set.mime_type.nil? # check if file_set is characterized
+          csv << [file_set.id, "Fileset not characterized", "Not fixed"]
+        else
+          solr_doc = SolrDocument.find(file_set.id)
+          if solr_doc.mime_type.nil? # when file_set is characterized but wasn't indexed properly
+            reindex_file_set(file_set, csv)
+          elsif solr_doc.thumbnail_path.to_s.start_with?("/assets/default-") # when file_set was characterized but thumbnail_path in solr doc is incorrect
+            regenerate_derivatives(file_set, csv)
+          end
+        end
+      end
+    end
+  end
+
+  private
+
+    def reindex_file_set(file_set, csv)
+      file_set.to_solr
+      file_set.save!
+      csv << [file_set.id, "Fileset not indexed", "Fixed"]
+    end
+
+    def regenerate_derivatives(file_set, csv)
+      preferred_file_symbol = file_set.preferred_file
+      preferred_file_uri = file_set.send(preferred_file_symbol).uri.to_s
+      asset_path = preferred_file_uri[preferred_file_uri.index(file_set.id.to_s)..-1]
+      CreateDerivativesJob.perform_later(file_set, asset_path)
+      csv << [file_set.id, "Thumbnail_path mismatch in solr_doc", "Queued"]
+    end
+end
--- spec/jobs/file_set_clean_up_job_spec.rb
+++ spec/jobs/file_set_clean_up_job_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe IndexFileSet, :clean do
+RSpec.describe FileSetCleanUpJob, :clean do
   let(:csv)         { IO.read(File.join("config/emory/index_file_set_results.csv")) }
   let(:file)        { File.open(fixture_path + '/book_page/0003_preservation_master.tif') }
   let(:file_set)    { FactoryBot.create(:file_set) }
@@ -10,7 +10,7 @@ RSpec.describe IndexFileSet, :clean do
     before do
       allow(SolrDocument).to receive_message_chain(:find, :mime_type)
       Hydra::Works::AddFileToFileSet.call(file_set, file, :preservation_master_file)
-      described_class.process
+      described_class.perform_now
     end
 
     it 'reindexes file_set' do
@@ -23,7 +23,7 @@ RSpec.describe IndexFileSet, :clean do
       allow(SolrDocument).to receive_message_chain(:find, :thumbnail_path).and_return("/assets/default-f936e9c3ea7a38e2c2092099586a71380b11258697b37fb4df376704495a849a.png")
       allow(SolrDocument).to receive_message_chain(:find, :mime_type).and_return('image/tiff')
       Hydra::Works::AddFileToFileSet.call(file_set, file, :preservation_master_file)
-      described_class.process
+      described_class.perform_now
     end
 
     it 'regenerates thumbnail and fixes thumbnail_path in solr' do
