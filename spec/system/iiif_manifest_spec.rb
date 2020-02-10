2752904bd4aa6a5eb8b703335930a6e80886eba9
Max Kadel <max@curationexperts.com>
2020-02-10 17:28:07 -0500

    Continue test for well-formed manifest

--- spec/system/iiif_manifest_spec.rb
+++ spec/system/iiif_manifest_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'rails_helper'
+require 'iiif_manifest'
 
 RSpec.describe 'viewing an IIIF manifest', type: :system do
   let(:ability)    { ::Ability.new(FactoryBot.create(:user)) }
@@ -14,8 +15,24 @@ RSpec.describe 'viewing an IIIF manifest', type: :system do
     it "has a well-formed manifest" do
       actor_stack_work
       work.reload
+
       visit "/concern/curate_generic_works/#{work.id}/manifest"
+
       expect(page.response_headers["Content-Type"]).to eq "application/json; charset=utf-8"
+
+      response_values = JSON.parse(page.body)
+
+      expect(response_values).to include "@context"
+      expect(response_values["@context"]).to include "http://iiif.io/api/presentation/2/context.json"
+      expect(response_values).to include "@type"
+      expect(response_values["@type"]).to include "sc:Manifest"
+      expect(response_values).to include "@id"
+      expect(response_values["@id"]).to include "/concern/curate_generic_works/#{work.id}/manifest"
+      expect(response_values).to include "label"
+      expect(response_values["label"]).to include work.title.first.to_s
+
+      # for continued work: per the 2.1 Presentation API, the manifest must include sequences,
+      # and the sequences must include canvases
     end
   end
 end
