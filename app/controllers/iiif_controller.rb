28a5782a760a617fb8f754a9e060fdd3abb60b9e
Bess Sadler <bess@curationexperts.com>
2020-02-12 10:20:10 -0500

    Emory Low View should never deliver more than 400 pixels on a side

--- app/controllers/iiif_controller.rb
+++ app/controllers/iiif_controller.rb
@@ -3,26 +3,30 @@
 # Proxy IIIF requests so we can protect IIIF images using Curate's rules around
 # object visibility.
 class IiifController < ApplicationController
-
   MAX_SIZE = ENV['MAX_LOW_RES_PIXELS'] || "400"
 
+  # Issue a IIIF image request to cantaloupe and return the result
   def show
-    @iiif_url = iiif_url
-    Rails.logger.info("Trying to proxy image from #{@iiif_url}")
+    check_for_required_environment_variables
+    @iiif_url = iiif_image_url
     return nil if @iiif_url.nil?
     send_data HTTP.get(@iiif_url).body, type: 'image/jpeg', x_sendfile: true, disposition: 'inline'
   end
 
+  # Issue an info.json request to cantaloupe and return the result
   def info
+    check_for_required_environment_variables
     @iiif_url = "#{ENV['PROXIED_IIIF_SERVER_URL']}#{trailing_slash_fix}#{identifier}/info.json"
-    Rails.logger.info("Trying to proxy info from #{@iiif_url}")
     send_data HTTP.get(@iiif_url).body, type: 'application/json', x_sendfile: true, disposition: 'inline'
+  end
+
+  def check_for_required_environment_variables
+    raise "PROXIED_IIIF_SERVER_URL must be set" unless ENV['PROXIED_IIIF_SERVER_URL']
   end
 
   # What should the IIIF query actually be?
   # It must be adjusted based on object visibility and whether the user is authenticated
-  def iiif_url
-    raise "PROXIED_IIIF_SERVER_URL must be set" unless ENV['PROXIED_IIIF_SERVER_URL']
+  def iiif_image_url
     visibility = visibility(identifier)
     return nil unless visibility
     adjusted_size = adjust_size(visibility: visibility, size: size)
@@ -30,14 +34,14 @@ class IiifController < ApplicationController
   end
 
   # Given a visibility setting and a size request, adjust the allowed size setting as appropriate
-  def adjust_size(visibility: , size: )
+  def adjust_size(visibility:, size:)
     return size if visibility == "open"
     return "#{MAX_SIZE}," if size == "full" && visibility == "low_res"
   end
 
   # Given a sha1 checksum, find the visibility of the FileSet
   def visibility(sha1)
-    response  = Blacklight.default_index.connection.get 'select', :params => {:q => "original_checksum_tesim:urn:sha1:#{sha1}"}[0]
+    response = Blacklight.default_index.connection.get 'select', params: { q: "original_checksum_tesim:urn:sha1:#{sha1}" }[0]
     response["response"]["docs"][0]["visibility_ssi"]
   rescue
     Rails.logger.error "Could not find visibility setting for sha1 value #{sha1}"
--- spec/controllers/iiif_controller_spec.rb
+++ spec/controllers/iiif_controller_spec.rb
@@ -38,91 +38,95 @@ RSpec.describe IiifController, type: :controller do
       solr.commit
     end
 
-    context "a request for info.json" do
-      let(:params) do
-        {
-          identifier: identifier,
-          info:       "info",
-          format:     "json"
-        }
-      end
-      let(:expected_iiif_url) { "http://127.0.0.1:8182/iiif/2/#{identifier}/info.json" }
-
-      before do
-        ENV['PROXIED_IIIF_SERVER_URL'] = 'http://127.0.0.1:8182/iiif/2'
-      end
-
-      it "returns as json" do
-        stub_request(:any, expected_iiif_url).to_return(
-          body:    "SUCCESS",
-          status:  200,
-          headers: { 'Content-Length' => 3 }
-        )
-        get :info, params: params
-        expect(assigns(:iiif_url)).to eq expected_iiif_url
-      end
-    end
-
-    describe "a request for a public object" do
+    context "public / open documents" do
       let(:identifier) { "7e7c52437c41c0ec1982e39d93a7a32db1ed1ba7" }
       let(:document) do
         {
-          id: '222333',
-          has_model_ssim:["FileSet"],
-          visibility_ssi: ['open'],
-          original_checksum_tesim:["urn:md5:7bd6a1468ed28a174c928a1423dd2704",
-            "urn:sha1:7e7c52437c41c0ec1982e39d93a7a32db1ed1ba7",
-            "urn:sha256:041bd2ea85c0cb8a41917b99f76ae4cd27108db887ce95fe4b0c83c5b16b1ed7"]
+          id:                      '222333',
+          has_model_ssim:          ["FileSet"],
+          visibility_ssi:          ['open'],
+          original_checksum_tesim: ["urn:md5:7bd6a1468ed28a174c928a1423dd2704",
+                                    "urn:sha1:#{identifier}",
+                                    "urn:sha256:041bd2ea85c0cb8a41917b99f76ae4cd27108db887ce95fe4b0c83c5b16b1ed7"]
         }
       end
-      let(:expected_iiif_url) { "http://127.0.0.1:8182/iiif/2/#{identifier}/full/full/0/default.jpg" }
+      context "a request for info.json" do
+        let(:params) do
+          {
+            identifier: identifier,
+            info:       "info",
+            format:     "json"
+          }
+        end
+        let(:expected_iiif_url) { "http://127.0.0.1:8182/iiif/2/#{identifier}/info.json" }
 
-      before do
-        ENV['PROXIED_IIIF_SERVER_URL'] = 'http://127.0.0.1:8182/iiif/2'
-        delete_all_documents_from_solr
-        solr = Blacklight.default_index.connection
-        solr.add([document])
-        solr.commit
+        it "returns as json" do
+          stub_request(:any, expected_iiif_url).to_return(
+            body:    "SUCCESS",
+            status:  200,
+            headers: { 'Content-Length' => 3 }
+          )
+          get :info, params: params
+          expect(assigns(:iiif_url)).to eq expected_iiif_url
+        end
       end
-
-      it "does not alter the iiif request" do
-        stub_request(:any, expected_iiif_url).to_return(
-          body:    "SUCCESS",
-          status:  200,
-          headers: { 'Content-Length' => 3 }
-        )
-        get :show, params: params
-        expect(assigns(:iiif_url)).to eq expected_iiif_url
+      context "a request for a public image" do
+        let(:expected_iiif_url) { "http://127.0.0.1:8182/iiif/2/#{identifier}/full/full/0/default.jpg" }
+        it "does not alter the iiif request" do
+          stub_request(:any, expected_iiif_url).to_return(
+            body:    "SUCCESS",
+            status:  200,
+            headers: { 'Content-Length' => 3 }
+          )
+          get :show, params: params
+          expect(assigns(:iiif_url)).to eq expected_iiif_url
+        end
       end
     end
 
-    describe "a request for a Public Low View object" do
-
+    context "a request for a Public Low View object" do
       let(:identifier) { "7e7c52437c41c0ec1982e39d93a7a32db1ed1ba7" }
       let(:document) do
         {
-          id: '222333',
-          has_model_ssim:["FileSet"],
-          visibility_ssi: ['low_res'],
-          original_checksum_tesim:["urn:md5:7bd6a1468ed28a174c928a1423dd2704",
-            "urn:sha1:7e7c52437c41c0ec1982e39d93a7a32db1ed1ba7",
-            "urn:sha256:041bd2ea85c0cb8a41917b99f76ae4cd27108db887ce95fe4b0c83c5b16b1ed7"]
+          id:                      '222333',
+          has_model_ssim:          ["FileSet"],
+          visibility_ssi:          ['low_res'],
+          original_checksum_tesim: ["urn:md5:7bd6a1468ed28a174c928a1423dd2704",
+                                    "urn:sha1:7e7c52437c41c0ec1982e39d93a7a32db1ed1ba7",
+                                    "urn:sha256:041bd2ea85c0cb8a41917b99f76ae4cd27108db887ce95fe4b0c83c5b16b1ed7"]
         }
       end
-      let(:expected_iiif_url) { "http://127.0.0.1:8182/iiif/2/#{identifier}/full/400,/0/default.jpg" }
+      let(:altered_iiif_url) { "http://127.0.0.1:8182/iiif/2/#{identifier}/full/400,/0/default.jpg" }
 
       it "alters the iiif request to ensure a max resolution" do
-        stub_request(:any, expected_iiif_url).to_return(
+        stub_request(:any, altered_iiif_url).to_return(
           body:    "SUCCESS",
           status:  200,
           headers: { 'Content-Length' => 3 }
         )
         get :show, params: params
-        expect(assigns(:iiif_url)).to eq expected_iiif_url
+        expect(assigns(:iiif_url)).to eq altered_iiif_url
       end
 
-      it 'fetches an info.json document' do
+      context "a request for info.json" do
+        let(:params) do
+          {
+            identifier: identifier,
+            info:       "info",
+            format:     "json"
+          }
+        end
+        let(:expected_iiif_url) { "http://127.0.0.1:8182/iiif/2/#{identifier}/info.json" }
 
+        it "returns as json" do
+          stub_request(:any, expected_iiif_url).to_return(
+            body:    "SUCCESS",
+            status:  200,
+            headers: { 'Content-Length' => 3 }
+          )
+          get :info, params: params
+          expect(assigns(:iiif_url)).to eq expected_iiif_url
+        end
       end
     end
   end
