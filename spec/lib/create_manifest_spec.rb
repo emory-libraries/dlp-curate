e8c95a833517d324e7c61a55c3ad49250f3bf359
devanshu-m <devanshu_m@gwmail.gwu.edu>
2020-02-16 18:15:16 -0500

    Adds env variable for manifest cache path
    * We are adding an env variable that will contain the path to the
    directory where manifests get cached. Path example:
    `full/path/to/directory/`

--- spec/lib/create_manifest_spec.rb
+++ spec/lib/create_manifest_spec.rb
@@ -19,6 +19,7 @@ RSpec.describe CreateManifest, :clean do
   let(:presenter) { described_class.new(solr_document, ability, request) }
 
   before do
+    ENV['IIIF_MANIFEST_CACHE'] = "./tmp/"
     allow(CurateGenericWork).to receive(:all).and_return([generic_work])
     allow(SolrDocument).to receive(:find).and_return(solr_document)
     FileUtils.rm_f("./tmp/2019-11-11_18-20-32_888888")
