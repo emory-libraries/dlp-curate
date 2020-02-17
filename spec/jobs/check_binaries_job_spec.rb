a1be3a8b21ba53472d881dd5d1eb9b427f433ea0
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2020-02-17 13:31:14 -0500

    Passes missing csv arg in check_binaries (#1003)
    * The csv was not being passed as an arg when running check_binaries
    job on all file_sets. This missing arg is being added to the method
    call and the definition. Tests are also updated for the same.

--- spec/jobs/check_binaries_job_spec.rb
+++ spec/jobs/check_binaries_job_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe CheckBinariesJob, :clean do
   let(:csv)           { IO.read(File.join("config/emory/check_binaries_results.csv")) }
   let(:file)          { File.open(fixture_path + '/book_page/0003_preservation_master.tif') }
   let(:file_set)      { FactoryBot.create(:file_set) }
+  let(:file_set2)     { FactoryBot.create(:file_set) }
   let(:generic_work)  { FactoryBot.create(:public_work) }
   let(:generic_work2) { FactoryBot.create(:public_work) }
 
@@ -13,6 +14,8 @@ RSpec.describe CheckBinariesJob, :clean do
     generic_work.save
     generic_work2.ordered_members << file_set
     generic_work2.save
+    generic_work.ordered_members << file_set2
+    generic_work.save
   end
 
   context "file is present in s3" do
@@ -36,6 +39,20 @@ RSpec.describe CheckBinariesJob, :clean do
 
     it "cannot find file in s3 and adds to csv" do
       expect(csv).to include("#{generic_work.id}|#{generic_work2.id},#{file_set.id},#{file_set.files.first.id},#{file_set.files.first.digest.first.to_s.partition('urn:sha1:').last}")
+    end
+  end
+
+  context "run job on all file_sets when file_set_id is nil" do
+    before do
+      Hydra::Works::AddFileToFileSet.call(file_set, file, :preservation_master_file)
+      Hydra::Works::AddFileToFileSet.call(file_set2, file, :preservation_master_file)
+      allow(Aws::S3::Resource).to receive_message_chain(:new, :bucket, :object, :exists?).and_return(false)
+      described_class.perform_now('example-bucket')
+    end
+
+    it "checks for all file_sets in s3" do
+      expect(csv).to eq("#{generic_work.id}|#{generic_work2.id},#{file_set.id},#{file_set.files.first.id},#{file_set.files.first.digest.first.to_s.partition('urn:sha1:').last}
+#{generic_work.id},#{file_set2.id},#{file_set2.files.first.id},#{file_set2.files.first.digest.first.to_s.partition('urn:sha1:').last}\n")
     end
   end
 end
