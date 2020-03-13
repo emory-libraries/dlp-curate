5aae2e26776278ff0235e36d76620db07a8bc214
Alex Zotov <alexandr.zotov@emory.edu>
2020-03-13 11:22:32 -0400

    adds functionality to pick a collection thumbnail

--- spec/system/edit_collection_spec.rb
+++ spec/system/edit_collection_spec.rb
@@ -4,8 +4,11 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.describe 'Edit an existing collection', :clean, type: :system, js: true do
-  let(:collection) { Collection.create!(collection_attrs) }
   let(:collection) { FactoryBot.create(:collection_lw, user: admin) }
+  let(:user) { FactoryBot.create(:user) }
+  let(:work1) { FactoryBot.build(:work, user: admin) }
+  let(:file_set) { FactoryBot.create(:file_set, user: admin, title: ["Test title"], pcdm_use: "Primary Content") }
+  let(:file) { File.open(fixture_path + '/sun.png') }
   let(:admin) { FactoryBot.create :admin }
 
   let(:collection_attrs) do
@@ -30,7 +33,10 @@ RSpec.describe 'Edit an existing collection', :clean, type: :system, js: true do
     collection_attrs.each do |k, v|
       collection.send((k.to_s + "=").to_sym, v)
     end
-    collection.save
+    Hydra::Works::AddFileToFileSet.call(file_set, file, :original_file)
+    work1.ordered_members << file_set
+    work1.member_of_collections << collection
+    work1.save!
   end
 
   context 'logged in as an admin user' do
@@ -44,14 +50,16 @@ RSpec.describe 'Edit an existing collection', :clean, type: :system, js: true do
       expect(find_field('abstract (Description/Abstract)').value).to eq 'Collection of photographs depicting African American life and culture collected by Robert Langmuir.'
       click_on 'Additional fields'
       expect(find_field('administrative_unit (Administrative Unit)').value).to eq 'Stuart A. Rose Manuscript, Archives, and Rare Book Library'
-
+      expect(page).to have_content 'Thumbnail'
+      first('#s2id_collection_thumbnail_id', minimum: 1).click
+      find('li.select2-result').click
+      first('body').click
       # Edit some fields in the form
       fill_in 'title (Title)', with: 'New Title'
-
       click_on 'Save changes'
-
       # Now the form should have the new values
       expect(page).to have_content 'New Title'
+      expect(page).to have_content file_set.id
     end
   end
 end
