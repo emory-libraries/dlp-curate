a7e8d6fbb0bfb622ee66f633983d81cdcc45ce66
Max Kadel <max@curationexperts.com>
2020-03-19 09:41:51 -0400

    Update token value

--- spec/services/iiif_auth_service_spec.rb
+++ spec/services/iiif_auth_service_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 
 RSpec.describe IiifAuthService do
   let(:service) { described_class.instance }
-  let(:encrypted_cookie_value) { "43BB3AA86080214273B978723D70DE6894DB9DEAC93FB27C79799EAD405B3FE8" }
+  let(:encrypted_cookie_value) { "BE0F7323469F3E7DF86CF9CA95B8ADD5D17753DA4F00BB67F2A9E8EC93E6A370" }
   let(:spoofed_cookie_value) { "5248ED788F7352EB9E4A8CE456EC238FEC70AEF70191246E424088EBC6364C1E" }
 
   describe 'decrypting cookie values' do
     it 'can decrypt a valid cookie string' do
-      expect(described_class.decrypt_cookie(encrypted_cookie_value)).to eq "This is a test token value"
+      expect(described_class.decrypt_cookie(encrypted_cookie_value)).to eq "This is a token value"
     end
 
     it 'does not raise an exception when trying to decrypt an invalid cookie string' do
