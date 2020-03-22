f0fad1736eb67dc84059ba1f48e9c5634b61ba26
Solomon Hilliard <solomon.hilliard@emory.edu>
2020-03-22 17:07:19 -0400

    add Env var for branding symblink path

--- config/deploy/localhost.rb
+++ config/deploy/localhost.rb
@@ -1,2 +1,4 @@
 # frozen_string_literal: true
+
+set :branding_symblink_path, ENV['BRANDING_SYMBLINK_PATH'] || 'unset'
 server '127.0.0.1', user: 'deploy', roles: [:web, :app, :db, :redhatapp, :collection]
