2c91aefd6dd41e6233b75b26ea7fa48959b61ee6
Max Kadel <max@curationexperts.com>
2020-04-28 12:11:02 -0400

    Should not show stack trace in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Attempt to read encrypted secrets from `config/secrets.yml.enc`.
