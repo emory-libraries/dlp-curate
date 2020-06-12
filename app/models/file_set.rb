efbb21cf04516730a2a4faef1baf177d4bd0d531
bwatson78 <brad.watson.orlando@gmail.com>
2020-06-12 08:55:03 -0500

    Corrects the namespace of Hyrax::VirusCheckerService

--- app/models/file_set.rb
+++ app/models/file_set.rb
@@ -35,7 +35,7 @@ class FileSet < ActiveFedora::Base
 
   include ::Hyrax::FileSetBehavior
   include PreservationEvents
-  include ::Hyrax::VirusCheckerService
+  include Hyrax::VirusCheckerService
   self.indexer = Curate::FileSetIndexer
 
   directly_contains_one :preservation_master_file, through: :files, type: ::RDF::URI('http://pcdm.org/use#PreservationMasterFile'), class_name: 'Hydra::PCDM::File'
