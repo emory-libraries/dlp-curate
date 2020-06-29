fdde1ea8ea425efea6af4460115bf6eb7e802d2f
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2020-06-29 15:47:45 -0400

    Adds missing preservation_event to its solr field (#1296)
    * Updates Hyrax to 3.0.0.pre.beta3
    
    Pins `rdf-vocab` to 3.1.4 to avoid an error preventing ActiveFedora from working.
    
    * Adds wings requirement (#1245)
    
    * Also corrects solr field name in collection behavior. We should now
    be able to run all rails commands and sidekiq.
    
    * upgrades controllers to beta 3 (#1274)
    
    Co-authored-by: Alex Zotov <alexandr.zotov@emory.edu>
    
    * Upgrades overwritten jobs to beta3 (#1258)
    
    * We are upgrading all previous overwritten files in `app/jobs`
    to hyrax3 beta3 with the new hyrax overwrite tag on top of files.
    * Also, updating collections factory since it was copied from
    the previous hyrax version.
    
    * Updates or removes initializers based on new versions. (#1254)
    
    * Adds wings requirement
    
    * Also corrects solr field name in collection behavior. We should now
    be able to run all rails commands and sidekiq.
    
    * Updates or removes initializers based on new versions.
    
    Co-authored-by: devanshu-m <devanshu_m@gwmail.gwu.edu>
    
    * Updates all other files for the hyrax3 beta3 bump up. (#1259)
    
    * Updates views for Hyrax3 Beta3 bump up. (#1238). (#1257)
    
    * Updates Hyrax to 3.0.0.pre.beta3
    
    Pins `rdf-vocab` to 3.1.4 to avoid an error preventing ActiveFedora from working.
    
    * Bank work
    
    * Updates views for Hyrax3 Beta3 bump up.
    
    * Carrying over wings requirement.
    
    Co-authored-by: Collin Brittle <cnbrittle@gmail.com>
    
    * Updates all model overrides and gem method calls to newer versions (#1239). (#1256)
    
    * Adds wings requirement
    
    * Also corrects solr field name in collection behavior. We should now
    be able to run all rails commands and sidekiq.
    
    * Updates all of the model overrides and gem method calls to newer versions (#1239).
    
    * Makes file include the VirusCheckerService class
    
    * Corrects the namespace of Hyrax::VirusCheckerService
    
    * merged hyrax3-beta3 into this branch
    
    * Corrects the VirusCheckerService call.
    
    Co-authored-by: devanshu-m <devanshu_m@gwmail.gwu.edu>
    
    * Adds size class to job_io_wrapper (#1276)
    
    * Adds size class to job_io_wrapper
    
    * Adds spec for job_io_wrapper's size instance method
    
    * Corrects fact that fileset with alternative is is never saved. (#1277)
    
    * Corrects fact that fileset with alternative is is never saved.
    
    * Fixes rubocop errors (#1279)
    
    * Controller spec update (#1278)
    
    * Updates required controller specs to make the suite greener.
    
    * Defines  as a property in both works and collections. (#1281)
    
    * Corrects errors caused by preservation workflow field ending with _sim instead of _tesim. (#1280)
    
    * Updates services spec (#1286)
    
    * spec/services/parent_query_service_spec.rb: Updates `SolrService`
    parent module to `Hyrax`.
    
    * Updates requests specs (#1287)
    
    * spec/requests/iiif_requests_spec.rb: Changes `User-Agent` to
    new version.
    
    * Adds /alt_title/ to the work attributes expectation. (#1285)
    
    * Updates forms spec (#1288)
    
    * spec/forms/hyrax/forms/collection_form_spec.rb: changes alt_title
    field to an array/makes it multi valued.
    
    * Updates collection spec (#1293)
    
    * app/forms/hyrax/forms/collection_form.rb: Removes `multiple`
    method which was previously causing error when loading collection
    form.
    * app/models/concerns/hyrax/collection_behavior.rb: Updates hyrax
    version.
    * spec/factories/collections.rb: updates collection title
    * spec/system/iiif_access_controls_spec.rb: Updates `User-Agent`
    
    * Adds missing preservation_event to its solr field
    
    * We are adding the missing preservation_events that appear as
    nested objects in fedora to the PE solr field so that we can
    display them on the file_set view page.
    
    Co-authored-by: Collin Brittle <cnbrittle@gmail.com>
    Co-authored-by: Alex Zotov <alexandr.zotov@emory.edu>
    Co-authored-by: Brad Watson <brad.watson.orlando@gmail.com>

--- spec/indexers/curate/file_set_indexer_spec.rb
+++ spec/indexers/curate/file_set_indexer_spec.rb
@@ -24,5 +24,28 @@ RSpec.describe Curate::FileSetIndexer, clean: true do
       expect(indexer['sha1_tesim']).to include(file_set.preservation_master_file.checksum.uri.to_s,
                                                file_set.service_file.checksum.uri.to_s)
     end
+
+    describe "#other_events" do
+      subject :other_events do
+        described_class.new(file_set).send(:other_events)
+      end
+
+      let(:event) { JSON.parse(other_events.first) }
+
+      before do
+        # we are emptying preservation_events so that we can call
+        # `other_events` method in the class and have it return the
+        # nested object rather than the preservation_event itself.
+        file_set.preservation_event = []
+        file_set.save!
+      end
+
+      it "saves nested object as preservation_event in solr" do
+        expect(event['event_details']).to eq(["No viruses found"])
+        expect(event['event_type']).to eq("Virus Check")
+        expect(event['software_version']).to eq("ClamAV 0.101.4")
+        expect(indexer['preservation_event_tesim'].first.first).to include("{\"event_details\":[\"No viruses found\"],\"event_type\":\"Virus Check\"")
+      end
+    end
   end
 end
