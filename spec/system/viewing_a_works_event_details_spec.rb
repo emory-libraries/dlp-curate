7c4207267d04ab50d73105d8a3fbde5c2e248eb1
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2020-07-01 13:30:38 -0400

    Adds info to event details page for works (#1305)
    * Adds info to event details page for works
    
    * We are adding preservation workflow and preservation event
    details to the work event details page.
    
    * Improves code readability and reusability

--- spec/system/viewing_a_works_event_details_spec.rb
+++ spec/system/viewing_a_works_event_details_spec.rb
@@ -3,16 +3,53 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.describe 'viewing a the event details page for a work', type: :system, clean: true do
+  include PreservationEvents
   let(:admin_user) { FactoryBot.create(:admin) }
-  let(:work) { FactoryBot.build(:work_with_full_metadata, user: admin_user) }
+  let(:work) { CurateGenericWork.where(deduplication_key: 'MSS1218_B071_I205').first }
+  let(:csv) { fixture_path + '/preservation_workflows.csv' }
+  let(:pe) do
+    {
+      'type' => 'Validation',
+      'start' => DateTime.current,
+      'outcome' => 'Success',
+      'details' => 'Example details',
+      'software_version' => 'Curate v.1',
+      'user' => 'userexample'
+    }
+  end
   before do
+    CurateGenericWork.create(title: ['Example title'], deduplication_key: 'MSS1218_B071_I205')
     login_as admin_user
+    PreservationWorkflowImporter.import(csv)
+    create_preservation_event(work, pe)
     work.save!
     work.reload
     visit "/concern/curate_generic_works/#{work.id}/event_details?locale=en"
   end
 
-  it "loads the page with a main title" do
-    expect(body).to have_content('View Preservation Details')
+  include_examples "check_page_for_multiple_text",
+    [
+      'View Preservation Details',
+      'Migrated to Cor repository from Extensis Portfolio DAMS',
+      'Scholarly Communications Office',
+      "This is a sample note. This field isn't always populated."
+    ],
+    'has preservation_workflow content'
+
+  it "loads the page with a main title and details" do
+    table_headers = all('#fs-preservation-event-table th').map(&:text)
+    expect(table_headers).to eq(["Event", "Timestamp", "Outcome", "Detail", "User", "Software"])
+    table_values = all('#fs-preservation-event-table td').map(&:text)
+    preservation_event = work.preservation_event.first
+    expect(table_values).to eq(
+      [
+        'Validation',
+        "Start: #{preservation_event.event_start.first} End: #{preservation_event.event_end.first}",
+        'Success',
+        'Example details',
+        'userexample',
+        'Curate v.1'
+      ]
+    )
   end
 end
