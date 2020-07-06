3e941c26564e44349f636601f2c8b9b6aa18e198
devanshu-m <devanshu_m@gwmail.gwu.edu>
2020-07-06 15:28:37 -0400

    Adds valkyrie overwrite version

--- config/initializers/rdf_uri_to_json.rb
+++ config/initializers/rdf_uri_to_json.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+# [Valkyrie-overwrite-v1.7.1] This is fixed in Valkyrie-v2.1.0. Remove this
+# when we upgrade to v2.1.0 or higher.
 ##
 # These patches are necessary for the postgres adapter to build JSON-LD versions
 # of RDF objects when `to_json` is called on them - that way they're stored in
