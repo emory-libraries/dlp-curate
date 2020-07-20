8fae50ba13782a5f09300df2615cef5d1b52967c
Will Ulman <will.ulman@gmail.com>
2020-07-20 12:10:24 -0400

    Fix system spec settings

--- spec/system/viewing_a_work_spec.rb
+++ spec/system/viewing_a_work_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.describe 'viewing the importer guide', type: :system, clean: true, js: true do
+RSpec.describe 'viewing the importer guide', type: :system, clean: true do
   let(:admin_user) { FactoryBot.create(:admin) }
   let(:work) { FactoryBot.build(:work_with_full_metadata, user: admin_user) }
   let(:user) { FactoryBot.create(:user) }
