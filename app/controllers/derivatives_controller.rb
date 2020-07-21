94ea54511a92660d2c9413999c1dffdbca2bc765
devanshu-m <devanshu_m@gwmail.gwu.edu>
2020-07-21 13:27:47 -0400

    Adds flash notice to derivative regen process so that users know to wait before retrying

--- app/controllers/derivatives_controller.rb
+++ app/controllers/derivatives_controller.rb
@@ -5,5 +5,7 @@ class DerivativesController < ApplicationController
 
   def clean_up
     FileSetCleanUpJob.perform_later(params[:file_set_id])
+    flash[:notice] = "Your update request has been submitted and may take several minutes to complete"
+    redirect_to hyrax_file_set_path(params[:file_set_id])
   end
 end
