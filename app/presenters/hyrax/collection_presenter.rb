736eb95601db709b95bccec2a46e2f6f36dd6493
bwatson78 <brad.watson.orlando@gmail.com>
2020-08-21 13:59:07 -0400

    Corrects the delay in showing Collection Index page.

--- app/presenters/hyrax/collection_presenter.rb
+++ app/presenters/hyrax/collection_presenter.rb
@@ -113,7 +113,7 @@ module Hyrax
     end
 
     def total_viewable_items
-      ActiveFedora::Base.where("member_of_collection_ids_ssim:#{id}").size
+      ActiveFedora::Base.where("member_of_collection_ids_ssim:#{id}").count
     end
 
     def total_viewable_works
