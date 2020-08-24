2372bcf6da16a4167c3a6aca1455bcce1b3798ba
bwatson78 <brad.watson.orlando@gmail.com>
2020-08-24 11:09:24 -0400

    Changes total_viewable_items to not care about viewability.

---
+++ app/presenters/hyrax/admin_set_presenter.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+# [Hyrax-overwrite-v3.0.0.pre.rc1]
+# Changes behavior of total_viewable_items to match total_items
+
+module Hyrax
+  class AdminSetPresenter < CollectionPresenter
+    ##
+    # @return [Boolean] true if there are items
+    def any_items?
+      total_items.positive?
+    end
+
+    def total_items
+      Hyrax::SolrService.count("{!field f=isPartOf_ssim}#{id}")
+    end
+
+    def total_viewable_items
+      total_items
+    end
+
+    # AdminSet cannot be deleted if default set or non-empty
+    def disable_delete?
+      AdminSet.default_set?(id) || any_items?
+    end
+
+    # Message to display if deletion is disabled
+    def disabled_message
+      return I18n.t('hyrax.admin.admin_sets.delete.error_default_set') if AdminSet.default_set?(id)
+
+      I18n.t('hyrax.admin.admin_sets.delete.error_not_empty') if any_items?
+    end
+
+    def collection_type
+      @collection_type ||= Hyrax::CollectionType.find_or_create_admin_set_type
+    end
+
+    def show_path
+      Hyrax::Engine.routes.url_helpers.admin_admin_set_path(id, locale: I18n.locale)
+    end
+
+    def available_parent_collections(*)
+      []
+    end
+
+    # For the Managed Collections tab, determine the label to use for the level of access the user has for this admin set.
+    # Checks from most permissive to most restrictive.
+    # @return String the access label (e.g. Manage, Deposit, View)
+    def managed_access
+      return I18n.t('hyrax.dashboard.my.collection_list.managed_access.manage') if current_ability.can?(:edit, solr_document)
+      return I18n.t('hyrax.dashboard.my.collection_list.managed_access.deposit') if current_ability.can?(:deposit, solr_document)
+      return I18n.t('hyrax.dashboard.my.collection_list.managed_access.view') if current_ability.can?(:read, solr_document)
+      ''
+    end
+
+    # Determine if the user can perform batch operations on this admin set.  Currently, the only
+    # batch operation allowed is deleting, so this is equivalent to checking if the user can delete
+    # the admin set determined by criteria...
+    # * user must be able to edit the admin set to be able to delete it
+    # * the admin set itself must be able to be deleted (i.e., there cannot be any works in the admin set)
+    # @return Boolean true if the user can perform batch actions; otherwise, false
+    def allow_batch?
+      return false unless current_ability.can?(:edit, solr_document)
+      !disable_delete?
+    end
+  end
+end
