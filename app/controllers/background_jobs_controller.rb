5a8715f9ed775c9660e1e381a5a4b4db52158d77
Alex Zotov <alexandr.zotov@emory.edu>
2020-09-09 12:12:10 -0500

    pulls path from rails root uploads folder

--- app/controllers/background_jobs_controller.rb
+++ app/controllers/background_jobs_controller.rb
@@ -9,7 +9,7 @@ class BackgroundJobsController < ApplicationController
       msg = "File Set Cleanup Job"
     elsif params[:jobs] == 'preservation'
       name = params[:preservation_csv].original_filename
-      path = File.join('tmp', 'csv_uploads', name)
+      path = Rails.root.join('tmp', 'uploads', name)
       File.open(path, "wb") { |f| f.write(params[:preservation_csv].read) }
       PreservationWorkflowImporterJob.perform_later(path)
       msg = "Preservation Workflow Importer Job"
