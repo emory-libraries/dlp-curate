3c45a9746fbf6bbadbafcbedba6c5145ba843096
bwatson78 <brad.watson.orlando@gmail.com>
2020-09-18 10:45:10 -0400

    Displays Source alongside Deposit Collection in Is Part Of on Dashboard Works (#1479).

---
+++ app/helpers/hyrax/collections_override_helper.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Hyrax
+  module CollectionsOverrideHelper
+    include Hyrax::CollectionsHelper
+
+    # rubocop:disable Rails/ContentTag
+    def render_collection_links(solr_doc)
+      collection_list = Hyrax::CollectionMemberService.run(solr_doc, controller.current_ability)
+      source_collection = collection_list.present? ? [::SolrDocument.find(collection_list.first['source_collection_id_tesim'])] : []
+      collection_list |= source_collection
+      return if collection_list.empty?
+      links = collection_list.map { |collection| link_to collection.title_or_label, hyrax.collection_path(collection.id) }
+      collection_links = []
+      links.each_with_index do |link, n|
+        collection_links << link
+        collection_links << ', ' unless links[n + 1].nil?
+      end
+      content_tag :span, safe_join([t('hyrax.collection.is_part_of'), ': '] + collection_links)
+    end
+    # rubocop:enable Rails/ContentTag
+  end
+end
