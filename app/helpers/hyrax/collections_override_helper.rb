c3b8b1caf8930ed7c9e95e72033f47867083275a
bwatson78 <brad.watson.orlando@gmail.com>
2020-09-18 13:29:07 -0400

    Adds testing and tries to appease CI Cognitive Complexity.

--- app/helpers/hyrax/collections_override_helper.rb
+++ app/helpers/hyrax/collections_override_helper.rb
@@ -7,9 +7,9 @@ module Hyrax
     # rubocop:disable Rails/ContentTag
     def render_collection_links(solr_doc)
       collection_list = Hyrax::CollectionMemberService.run(solr_doc, controller.current_ability)
-      source_collection = collection_list.present? ? [::SolrDocument.find(collection_list.first['source_collection_id_tesim'])] : []
-      collection_list |= source_collection
       return if collection_list.empty?
+      source_collection = collection_list.first['source_collection_id_tesim'].present? ? [::SolrDocument.find(collection_list.first['source_collection_id_tesim'])] : []
+      collection_list |= source_collection
       links = collection_list.map { |collection| link_to collection.title_or_label, hyrax.collection_path(collection.id) }
       collection_links = []
       links.each_with_index do |link, n|
