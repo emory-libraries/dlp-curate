f73d747dfd5938b42f86c025dcc2fa865f76ba3f
bwatson78 <brad.watson.orlando@gmail.com>
2020-10-15 10:23:37 -0400

    Removes two unnecessary links from the homepage (#1511).

--- spec/system/viewing_homepage_spec.rb
+++ spec/system/viewing_homepage_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.describe 'viewing the homepage', type: :system do
-  let(:user) { FactoryBot.create(:user) }
+  let(:user) { FactoryBot.create(:admin) }
+
+  before do
+    login_as user
+    visit root_path
+  end
 
   context 'when contact link clicked' do
     it 'goes to the libwizard link' do
-      login_as user
-      visit root_path
-
       expect(find_link('Contact')[:href]).to eq 'https://emory.libwizard.com/f/dlp-feedback'
       expect(find_link('Contact')[:target]).to eq '_blank'
     end
+  end
+
+  it "doesn't show Terms of Use or Share Your Work links" do
+    expect(page).not_to have_link('Share Your Work')
+    expect(page).not_to have_link('Terms of Use')
   end
 end
