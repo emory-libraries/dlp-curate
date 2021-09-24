6afe1d6d722480e4793e30549b05fce9f1e367bb
Devanshu Matlawala <devanshu.matlawala@emory.edu>
2021-09-24 08:46:10 -0400

    Updates collection_behavior and its respective spec. Also updates collection_nesting to match new hyrax version (#1660)

---
+++ spec/models/concerns/hyrax/collection_behavior_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# [Hyrax-overwrite-v3.1.0]
+require 'rails_helper'
+
+RSpec.describe Hyrax::CollectionBehavior, clean_repo: true do
+  let(:collection) { FactoryBot.create(:collection_lw) }
+  let(:work) { FactoryBot.valkyrie_create(:hyrax_work) }
+
+  describe "#destroy" do
+    it "removes the collection id from associated members" do
+      Hyrax::Collections::CollectionMemberService.add_members(collection_id: collection.id,
+                                                              new_members: [work],
+                                                              user: nil)
+      collection.save
+
+      collection_via_query = Hyrax.query_service.find_by_alternate_identifier(alternate_identifier: collection.id, use_valkyrie: false)
+
+      expect { collection_via_query.destroy }
+        .to change { Hyrax.query_service.find_by(id: work.id).member_of_collection_ids }
+        .from([collection.id])
+        .to([])
+    end
+  end
+end
