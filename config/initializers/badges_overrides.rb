2fb3af866dcdd2d6a3b0a04c8a747bb5c90694b6
bwatson78 <brad.watson.orlando@gmail.com>
2021-10-01 06:29:14 -0400

    Fixes prepend loading issues for visibility (#1680).

---
+++ config/initializers/badges_overrides.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# [Hyrax-overwrite-v3.1.0] injects custom Globals for access rights.
+Hyrax::PermissionBadge.class_eval do
+  def text
+    I18n.t("hyrax.visibility.#{@visibility}.text")
+  rescue
+    @visibility.to_s
+  end
+end
