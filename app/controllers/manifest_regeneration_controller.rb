42d27c45a186ebd12fccc383ed13b74cc24e6080
Brad Watson <brad.watson.orlando@gmail.com>
2021-10-04 11:11:47 -0400

    Bug: eliminates duplicate Manifest Regeneration jobs (#1637). (#1641)

--- app/controllers/manifest_regeneration_controller.rb
+++ app/controllers/manifest_regeneration_controller.rb
@@ -9,7 +9,6 @@ class ManifestRegenerationController < ApplicationController
     key = solr_doc[:manifest_cache_key_tesim]&.first.to_s + '_' + params[:work_id]
     file_path = File.join(iiif_manifest_cache, key)
     File.delete(file_path) if File.exist?(file_path)
-    ManifestBuilderService.build_manifest(presenter: presenter(solr_doc), curation_concern: CurateGenericWork.find(params[:work_id]))
     redirect_to hyrax_curate_generic_work_path(params[:work_id])
   end
 
