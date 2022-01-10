a85d939eb16aa19976b568b44b0b1667e2abaadf
bwatson78 <brad.watson.orlando@gmail.com>
2022-01-10 14:37:49 -0500

    fixes webpacker?

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,14 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
 
 module.exports = environment
-
-environment.plugins.prepend(
-  'Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    jquery: 'jquery',
-    Popper: ['popper.js', 'default']
-  })
-)
