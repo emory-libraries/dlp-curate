936fb83f9a27791b119cdad00c4d606786339f82
bwatson78 <brad.watson.orlando@gmail.com>
2022-02-03 08:41:55 -0500

    Codeclimate tweak and form correction.

--- app/forms/hyrax/curate_generic_work_form.rb
+++ app/forms/hyrax/curate_generic_work_form.rb
@@ -22,6 +22,35 @@ module Hyrax
 
     self.required_fields = REQUIRED_FIELDS_ON_FORM
 
+    # Cast back to multi-value when saving
+    # Reads from form
+    def self.model_attributes(attributes)
+      attrs = super
+      return attrs unless attributes[:title]
+
+      attrs[:title] = Array(attributes[:title])
+      return attrs if attributes[:alt_title].nil?
+      Array(attributes[:alt_title]).each do |value|
+        attrs["title"] << value if value != ""
+      end
+      attrs
+    end
+
+    # @param [Symbol] key the field to read
+    # @return the value of the form field.
+    # For display in edit page
+    def [](key)
+      return model.member_of_collection_ids if key == :member_of_collection_ids
+      if key == :title
+        @attributes["title"].each do |value|
+          @attributes["alt_title"] << value
+        end
+        @attributes["alt_title"].delete(@attributes["alt_title"].sort.first) unless @attributes["alt_title"].empty?
+        return @attributes[key.to_s].sort.first
+      end
+      super
+    end
+
     def primary_descriptive_metadata_fields
       [:title, :holding_repository, :date_created, :content_type, :content_genres, :administrative_unit, :creator, :contributors,
        :abstract, :primary_language, :date_issued, :extent, :sublocation]
