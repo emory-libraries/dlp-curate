8ed616741f85862678f86aef81e8d421c87a9a13
bwatson78 <brad.watson.orlando@gmail.com>
2022-03-10 12:32:49 -0500

    Codeclimate tweak.

--- app/jobs/process_aws_fixity_preservation_events_job.rb
+++ app/jobs/process_aws_fixity_preservation_events_job.rb
@@ -7,21 +7,19 @@ class ProcessAwsFixityPreservationEventsJob < Hyrax::ApplicationJob
     lines = CSV.read(csv, headers: true)
 
     Rails.logger.info "AWS Fixity Event processing of #{csv} started at #{DateTime.current}"
-    process_lines(lines)
+    lines.each { |l| process_line(l) }
     Rails.logger.info "AWS Fixity Event processing of #{csv} ended at #{DateTime.current}"
   end
 
   private
 
-    def process_lines(lines)
-      lines.each do |l|
-        event_obj = AwsFixityEvent.new(l)
-        next if event_obj.sha1.blank?
-        file_set = FileSet.where(sha1_tesim: event_obj.sha1)&.first
-        next if file_set.blank?
-        event = event_obj.process_event
+    def process_line(line)
+      event_obj = AwsFixityEvent.new(line)
+      return if event_obj.sha1.blank?
+      file_set = FileSet.where(sha1_tesim: event_obj.sha1)&.first
+      return if file_set.blank?
+      event = event_obj.process_event
 
-        create_preservation_event(file_set, event)
-      end
+      create_preservation_event(file_set, event)
     end
 end
