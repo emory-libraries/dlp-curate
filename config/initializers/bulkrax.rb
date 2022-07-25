930a09fe60a650e795120ed7ac57374c56e6c446
bwatson78 <brad.watson.orlando@gmail.com>
2022-07-25 07:10:01 -0400

    First commit of the redo.

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -8,10 +8,10 @@ Bulkrax.setup do |config|
 
   # WorkType to use as the default if none is specified in the import
   # Default is the first returned by Hyrax.config.curation_concerns
-  # config.default_work_type = MyWork
+  config.default_work_type = 'FileSet'
 
   # Path to store pending imports
-  # config.import_path = 'tmp/imports'
+  config.import_path = 'imports'
 
   # Path to store exports before download
   # config.export_path = 'tmp/exports'
@@ -30,9 +30,25 @@ Bulkrax.setup do |config|
 
   # Field mappings
   # Create a completely new set of mappings by replacing the whole set as follows
-  #   config.field_mappings = {
-  #     "Bulkrax::OaiDcParser" => { **individual field mappings go here*** }
-  #   }
+  config.field_mappings = {
+    "Bulkrax::CsvParser" => {
+      "title" => { from: ["title"], parsed: true },
+      "holding_repository" => { from: ["holding_repository"] },
+      "date_created" => { from: ["date_created"] },
+      "content_type" => { from: ["content_type"], parsed: true },
+      "emory_rights_statements" => { from: ["emory_rights_statements"] },
+      "rights_statement" => { from: ["rights_statement"], parsed: true },
+      "data_classifications" => { from: ["data_classifications"], parsed: true, split: '\|' },
+      "visibility" => { from: ["visibility"], parsed: true },
+      "deduplication_key" => { from: ["deduplication_key"], source_identifier: true },
+      "source_collection_id" => { from: ["source_collection_id"] },
+      "pcdm_use" => { from: ["pcdm_use"], parsed: true },
+      "file" => { from: ["file"] },
+      "parent" => { from: ["parent"], related_parents_field_mapping: true },
+      "model" => { from: ["model"] },
+      "file_types" => { from: ["file_types"], split: '\|' }
+    }
+  }
 
   # Add to, or change existing mappings as follows
   #   e.g. to exclude date
@@ -55,9 +71,7 @@ Bulkrax.setup do |config|
   # and download errored entries to still work, but does mean if you upload the
   # same source record in two different files you WILL get duplicates.
   # It is given two aruguments, self at the time of call and the index of the reocrd
-  #    config.fill_in_blank_source_identifiers = ->(parser, index) { "b-#{parser.importer.id}-#{index}"}
-  # or use a uuid
-  #    config.fill_in_blank_source_identifiers = ->(parser, index) { SecureRandom.uuid }
+  config.fill_in_blank_source_identifiers = ->(obj, index) { "Curate-#{obj.importerexporter.id}-#{index}" }
 
   # Properties that should not be used in imports/exports. They are reserved for use by Hyrax.
   # config.reserved_properties += ['my_field']
@@ -72,4 +86,88 @@ end
 # Sidebar for hyrax 3+ support
 if Object.const_defined?(:Hyrax) && ::Hyrax::DashboardController&.respond_to?(:sidebar_partials)
   Hyrax::DashboardController.sidebar_partials[:repository_content] << "hyrax/dashboard/sidebar/bulkrax_sidebar_additions"
+end
+
+require_relative '../../lib/importing_modules/file_set_methods'
+
+# rubocop:disable Metrics/BlockLength
+Bulkrax::ObjectFactory.class_eval do
+  include FileSetMethods
+  attr_reader :attributes, :object, :source_identifier_value, :klass, :replace_files, :update_files, :work_identifier, :related_parents_parsed_mapping, :importer_run_id, :parser
+
+  # Bulkrax v4.2.1 Override: as mentioned below, this method's largely mimicing AttachFilesToWorkJob,
+  #   which we have extensively customized in Curate to accomodate our needs. Here,
+  #   we are purely adapting those customizations.
+  #   TODO: To DRY up this code, we should shoot for refactoring these processes into
+  #   a reusable module.
+  # This method is heavily inspired by Hyrax's AttachFilesToWorkJob
+  def create_file_set(attrs)
+    _, work = find_record(attributes[related_parents_parsed_mapping].first, importer_run_id)
+    work_permissions = work.permissions.map(&:to_hash)
+    attrs = clean_attrs(attrs)
+    file_set_attrs = attrs.slice(*object.attributes.keys)
+    object.assign_attributes(file_set_attrs)
+
+    attrs['uploaded_files'].each do |uploaded_file_id|
+      uploaded_file = ::Hyrax::UploadedFile.find(uploaded_file_id)
+      next if uploaded_file.file_set_uri.present?
+
+      process_uploaded_file(uploaded_file, work, work_permissions, file_set_attrs)
+    end
+
+    object.save!
+  end
+
+  # Overridden because we need parser to process filetypes.
+  # rubocop:disable Metrics/ParameterLists
+  def initialize(
+    attributes:,
+    source_identifier_value:,
+    work_identifier:,
+    parser:,
+    related_parents_parsed_mapping: nil,
+    replace_files:                  false,
+    user:                           nil,
+    klass:                          nil,
+    importer_run_id:                nil,
+    update_files:                   false
+  )
+    @attributes = ActiveSupport::HashWithIndifferentAccess.new(attributes)
+    @replace_files = replace_files
+    @update_files = update_files
+    @user = user || User.batch_user
+    @work_identifier = work_identifier
+    @related_parents_parsed_mapping = related_parents_parsed_mapping
+    @source_identifier_value = source_identifier_value
+    @klass = klass || Bulkrax.default_work_type.constantize
+    @importer_run_id = importer_run_id
+    @parser = parser
+  end
+  # rubocop:enable Metrics/ParameterLists
+
+  # This overrides the method included in this class by the module Bulkrax::FileFactory.
+  def import_file(path)
+    u = Hyrax::UploadedFile.new
+    u.user_id = @user.id
+    u.send("#{process_file_types(path.split('/').last)}=", CarrierWave::SanitizedFile.new(path))
+    update_filesets(u)
+  end
+end
+# rubocop:enable Metrics/BlockLength
+
+Bulkrax::Entry.class_eval do
+  def factory
+    @factory ||= Bulkrax::ObjectFactory.new(
+      attributes:                     parsed_metadata,
+      source_identifier_value:        identifier,
+      work_identifier:                parser.work_identifier,
+      related_parents_parsed_mapping: parser.related_parents_parsed_mapping,
+      replace_files:                  replace_files,
+      user:                           user,
+      klass:                          factory_class,
+      importer_run_id:                importerexporter.last_run.id,
+      update_files:                   update_files,
+      parser:                         parser
+    )
+  end
 end
---
+++ lib/importing_modules/file_set_methods.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module FileSetMethods
+  def process_uploaded_file(uploaded_file, work, work_permissions, file_set_attrs)
+    actor = ::Hyrax::Actors::FileSetActor.new(object, @user)
+
+    uploaded_file.update(file_set_uri: actor.file_set.uri)
+    actor.file_set.permissions_attributes = work_permissions
+    actor.create_metadata(uploaded_file.fileset_use, file_set_attrs)
+    actor.fileset_name(uploaded_file.file.to_s) if uploaded_file.file.present?
+    create_content_for_actor(actor, uploaded_file)
+    work.ordered_members << actor.file_set
+    work.save
+    actor.file_set.save
+    actor.attach_to_work(work)
+  end
+
+  def create_content_for_actor(actor, uploaded_file)
+    preferred = preferred_file(uploaded_file)
+
+    actor.create_content(uploaded_file.preservation_master_file, preferred, :preservation_master_file)
+    actor.create_content(uploaded_file.intermediate_file, preferred, :intermediate_file) if uploaded_file.intermediate_file.present?
+    actor.create_content(uploaded_file.service_file, preferred, :service_file) if uploaded_file.service_file.present?
+    actor.create_content(uploaded_file.extracted_text, preferred, :extracted) if uploaded_file.extracted_text.present?
+    actor.create_content(uploaded_file.transcript, preferred, :transcript_file) if uploaded_file.transcript.present?
+  end
+
+  def preferred_file(uploaded_file)
+    preferred = if uploaded_file.service_file.present?
+                  :service_file
+                elsif uploaded_file.intermediate_file.present?
+                  :intermediate_file
+                else
+                  :preservation_master_file
+                end
+    preferred
+  end
+
+  def process_file_types(file_name)
+    raw_strings = parser.file_sets.map { |v| v[:file_types] }
+    split_strings = raw_strings.map { |rs| rs.split('|') }&.flatten
+    string_type_hashes = process_string_type_hashes(split_strings)
+    pulled_hash = string_type_hashes.select { |h| h[file_name].present? }&.first
+
+    pulled_hash[file_name] || "preservation_master_file"
+  end
+
+  def process_string_type_hashes(split_strings)
+    split_strings.map do |ss|
+      pieces = ss.split(':')
+      { pieces[0].to_s => pieces[1] }
+    end
+  end
+end
