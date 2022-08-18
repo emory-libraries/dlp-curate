ac23241305a93c89622b34a63c7a54c42d75816f
Brad Watson <brad.watson.orlando@gmail.com>
2022-08-18 14:37:56 -0400

    Updates the version number of Hyrax::FixityChecksController (#1866). (#1883)

--- app/controllers/hyrax/fixity_checks_controller.rb
+++ app/controllers/hyrax/fixity_checks_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-# [Hyrax-overwrite-v3.0.0.pre.rc1]
+# [Hyrax-overwrite-v3.4.1]
 # Adds redirect on L#28 in create action
 module Hyrax
   class FixityChecksController < ApplicationController
