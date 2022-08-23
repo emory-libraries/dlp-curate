03d8fca0e8ffe53a1e29f7fb0ba4341702dd9901
bwatson78 <brad.watson.orlando@gmail.com>
2022-08-23 13:20:16 -0400

    Updates the affected methods of Hyrax::Forms::Admin::CollectionTypeForm (#1862).

--- app/forms/hyrax/forms/admin/collection_type_form.rb
+++ app/forms/hyrax/forms/admin/collection_type_form.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# [Hyrax-overwrite-v3.0.0.pre.rc1] Adds new field to delegation.
+# [Hyrax-overwrite-v3.4.1] Adds new field to delegation.
 module Hyrax
   module Forms
     module Admin
@@ -14,16 +14,26 @@ module Hyrax
         delegate :title, :description, :brandable, :discoverable, :nestable, :sharable,
                  :share_applies_to_new_works, :require_membership, :allow_multiple_membership,
                  :assigns_workflow, :assigns_visibility, :id, :collection_type_participants,
-                 :persisted?, :collections?, :admin_set?, :user_collection?, :badge_color,
+                 :persisted?, :admin_set?, :user_collection?, :badge_color,
                  :deposit_only_collection,
                  to: :collection_type
 
+        ##
+        # @return [Boolean]
         def all_settings_disabled?
           collections? || admin_set? || user_collection?
         end
 
+        ##
+        # @return [Boolean]
         def share_options_disabled?
           all_settings_disabled? || !sharable
+        end
+
+        ##
+        # @return [Boolean]
+        def collections?
+          collection_type.collections.any?
         end
       end
     end
