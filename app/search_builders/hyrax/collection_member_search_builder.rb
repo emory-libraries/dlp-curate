9e0c594b634c6ed41cef8907b35cb312309b1bd0
Brad Watson <brad.watson.orlando@gmail.com>
2022-08-24 09:01:42 -0400

    Updates the affected methods of Hyrax::CollectionMemberSearchBuilder (#1862). (#1909)
    * Updates the affected methods of Hyrax::CollectionMemberSearchBuilder (#1862).
    
    * make rubocop happy.

--- app/search_builders/hyrax/collection_member_search_builder.rb
+++ app/search_builders/hyrax/collection_member_search_builder.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-# [Hyrax-overwrite-v3.0.0.pre.rc1] Modify #member_of_collection to also search against a source collection's deposit collections
+# [Hyrax-overwrite-v3.4.1] Modify #member_of_collection to also search against a source collection's deposit collections
 # Change below was necessary to institute Source/Deposit Collection structure.
 # For more information, read the SOURCE_DEPOSIT_CHANGES_README.md in dlp-curate's root folder.
 module Hyrax
   # This search builder requires that a accessor named "collection" exists in the scope
-  class CollectionMemberSearchBuilder < ::SearchBuilder
+  class CollectionMemberSearchBuilder < ::Hyrax::CollectionSearchBuilder
     include Hyrax::FilterByType
     attr_reader :collection, :search_includes_models
 
@@ -15,14 +15,28 @@ module Hyrax
     # Defines which search_params_logic should be used when searching for Collection members
     self.default_processor_chain += [:member_of_collection]
 
-    # @param [scope] Typically the controller object
-    # @param [Symbol] :works, :collections, (anything else retrieves both)
-    def initialize(scope:,
-                   collection:,
-                   search_includes_models: :works)
+    # @param [Object] scope Typically the controller object
+    # @param [Symbol] search_includes_models +:works+ or +:collections+; (anything else retrieves both)
+    def initialize(*args,
+                   scope: nil,
+                   collection: nil,
+                   search_includes_models: nil)
       @collection = collection
       @search_includes_models = search_includes_models
-      super(scope)
+
+      if args.any?
+        super(*args)
+      else
+        super(scope)
+      end
+    end
+
+    def collection
+      @collection || (scope.context[:collection] if scope&.respond_to?(:context))
+    end
+
+    def search_includes_models
+      @search_includes_models || :works
     end
 
     # include filters into the query to only include the collection memebers
