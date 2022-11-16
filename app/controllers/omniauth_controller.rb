f1922aaca40190e8a2821304cbd8f49325da77ca
bwatson78 <brad.watson.orlando@gmail.com>
2022-11-16 11:52:11 -0500

    Possible Shibboleth fix.

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -4,7 +4,7 @@ class OmniauthController < Devise::SessionsController
   def new
     # Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
     if Rails.env.production?
-      redirect_to user_shibboleth_omniauth_authorize_path
+      redirect_post user_shibboleth_omniauth_authorize_path
     else
       super
     end
