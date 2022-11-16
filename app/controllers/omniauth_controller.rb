5f6e63e49e4f7b00fcd119b8fd57a074ba893d9b
bwatson78 <brad.watson.orlando@gmail.com>
2022-11-16 12:01:26 -0500

    a tweak.

--- app/controllers/omniauth_controller.rb
+++ app/controllers/omniauth_controller.rb
@@ -4,7 +4,7 @@ class OmniauthController < Devise::SessionsController
   def new
     # Rails.logger.debug "SessionsController#new: request.referer = #{request.referer}"
     if Rails.env.production?
-      redirect_post user_shibboleth_omniauth_authorize_path
+      redirect_post(user_shibboleth_omniauth_authorize_path, options: {authenticity_token: :auto})
     else
       super
     end
