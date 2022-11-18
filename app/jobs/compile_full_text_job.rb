1ec1ab854aa880964bc181bd287a4901a915c714
Ayoub Belemlih <13107510+abelemlih@users.noreply.github.com>
2022-11-18 13:16:38 -0600

    Refactor compile_full_text_job.rb

--- app/jobs/compile_full_text_job.rb
+++ app/jobs/compile_full_text_job.rb
@@ -3,24 +3,26 @@
 class CompileFullTextJob < Hyrax::ApplicationJob
   def perform(work_id:, user_id:)
     work = CurateGenericWork.find(work_id)
-    path = generate_full_text_data_file_from(work: work)
+
+    begin
+      path = generate_full_text_data_file_from!(work: work)
+    rescue => e
+      Rails.logger.error("Unable to generate full text data for work #{work_id} due to the following error: #{e.message}")
+    end
+
     file = File.open(path)
     user = User.find(user_id)
-    file_set = FileSet.create(label: "Full Text Data - #{work_id}")
-    actor = Hyrax::Actors::FileSetActor.new(file_set, user)
-    actor.create_metadata(nil, {})
-    actor.create_content(file, :transcript_file, :transcript_file)
-    work.ordered_members << actor.file_set
-    work.save
-    actor.file_set.save
+    generate_file_set_from(file: file, work: work, user: user)
   end
 
   private
 
-    def generate_full_text_data_file_from(work:)
-      path = Rails.root.join('tmp', "full_text_data_#{work.id}")
+    def generate_full_text_data_file_from!(work:)
+      path = Rails.root.join('tmp', "full_text_data_#{work.id}.txt")
       File.open(path, "wb+") { |f| f.puts("") }
-      work.file_sets.map(&:transcript_file).compact.each do |transcript_file|
+      file_sets = CurateGenericWork.last.file_sets.filter { |fs| fs.label&.starts_with?('Page') }
+      file_sets = file_sets.sort_by { |fs| fs.label.partition(' ').last.to_i }
+      file_sets.map(&:transcript_file).compact.each do |transcript_file|
         next if transcript_file.content == "" || transcript_file.content == "[NO TEXT ON PAGE. This page does not contain any text recoverable by the OCR engine.]\n"
 
         File.open(path, "ab") do |f|
@@ -28,5 +30,15 @@ class CompileFullTextJob < Hyrax::ApplicationJob
         end
       end
       path
+    end
+
+    def generate_file_set_from(file:, work:, user:)
+      file_set = FileSet.create(label: "Full Text Data - #{work.id}")
+      actor = Hyrax::Actors::FileSetActor.new(file_set, user)
+      actor.create_metadata(nil, {})
+      actor.create_content(file, :transcript_file, :transcript_file)
+      work.ordered_members << actor.file_set
+      work.save
+      actor.file_set.save
     end
 end
--- spec/jobs/compile_full_text_job_spec.rb
+++ spec/jobs/compile_full_text_job_spec.rb
@@ -3,26 +3,34 @@
 require 'rails_helper'
 
 RSpec.describe CompileFullTextJob, :clean, perform_enqueued: [CompileFullTextJob, AttachFilesToWorkJob, IngestJob] do
-  let(:pmf) { File.open(fixture_path + '/book_page/0003_preservation_master.tif') }
-  let(:tf)  { File.open(fixture_path + '/book_page/0003_transcript.txt') }
-  let(:uploaded_file) do
+  let!(:uploaded_file_1) do
     FactoryBot.build(:uploaded_file,
-                     file:                     'Example title',
-                     preservation_master_file: pmf,
-                     transcript:               tf,
+                     file:                     'Page 1',
+                     preservation_master_file: File.open(fixture_path + '/full_text_data/page_1_pmf.tif'),
+                     transcript:               File.open(fixture_path + '/full_text_data/page_1_transcript.txt'),
                      fileset_use:              'primary')
   end
+
+  let!(:uploaded_file_2) do
+    FactoryBot.build(:uploaded_file,
+                     file:                     'Page 2',
+                     preservation_master_file: File.open(fixture_path + '/full_text_data/page_2_pmf.tif'),
+                     transcript:               File.open(fixture_path + '/full_text_data/page_2_transcript.txt'),
+                     fileset_use:              'primary')
+  end
+
   let(:generic_work) { FactoryBot.create(:public_generic_work) }
   let(:user)         { FactoryBot.create(:user) }
 
   it 'compiles full text into one file at the volume level' do
-    AttachFilesToWorkJob.perform_now(generic_work, [uploaded_file])
+    AttachFilesToWorkJob.perform_now(generic_work, [uploaded_file_1, uploaded_file_2])
     described_class.perform_now(work_id: generic_work.id, user_id: user.id)
     generic_work.reload
-    expect(generic_work.file_sets.count).to eq(2)
+    expect(generic_work.file_sets.count).to eq(3)
     full_text_file_set = generic_work.file_sets.last
     expect(full_text_file_set.parent).to eq(generic_work)
     expect(full_text_file_set.label).to eq "Full Text Data - #{generic_work.id}"
-    expect(full_text_file_set.files.first.content).to include("The idea of publishing cheap one-volume novels is a good one")
+    expected_text = "\nThis is page 1 of a transcript file used to test compiling full text data.\nThis is page 2 of a transcript file used to test compiling full text data.\n"
+    expect(full_text_file_set.files.first.content).to eq(expected_text)
   end
 end
