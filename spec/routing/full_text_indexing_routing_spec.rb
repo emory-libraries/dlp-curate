2d7795c6aad47bcc32b64a21cdabe1015ba869ea
bwatson78 <brad.watson.orlando@gmail.com>
2023-08-10 10:20:44 -0500

    Add testing.

---
+++ spec/routing/full_text_indexing_routing_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "routes for full-text indexing", type: :routing do
+  it "routes full-text indexing requests to the full_text_indexing#full_text_index controller" do
+    expect(post("/concern/curate_generic_works/1/full_text_index?user_id=1"))
+      .to route_to(
+        "controller" => "full_text_indexing",
+        "action" => "full_text_index",
+        "work_id" => "1",
+        "user_id" => "1"
+      )
+  end
+end
