8ef8c8524570fdd3fd46110ac1a5c4c0f2049eee
bwatson78 <brad.watson.orlando@gmail.com>
2024-12-19 12:16:40 -0600

    Tries to forgo auth.

--- config/initializers/blacklight_iiif_search_controller_overrides.rb
+++ config/initializers/blacklight_iiif_search_controller_overrides.rb
@@ -2,13 +2,5 @@
 
 # [BlacklightIiifSearch-overwrite-v1.0.0] sets 'Access-Control-Allow-Origin' before sending response.
 BlacklightIiifSearch::Controller.class_eval do
-  def iiif_search
-    _parent_response, @parent_document = fetch(params[:solr_document_id])
-    iiif_search = BlacklightIiifSearch::IiifSearch.new(iiif_search_params, iiif_search_config, @parent_document)
-    @response, _document_list = search_results(iiif_search.solr_params)
-    iiif_search_response = BlacklightIiifSearch::IiifSearchResponse.new(@response, @parent_document, self)
-    render json:         iiif_search_response.annotation_list,
-           content_type: 'application/json',
-           status:       :ok
-  end
+   skip_before_action :authenticate_user!
 end
