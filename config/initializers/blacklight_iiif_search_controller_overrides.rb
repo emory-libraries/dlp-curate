48c5d11ca53c6390d039aa510d62f7d94c90467f
bwatson78 <brad.watson.orlando@gmail.com>
2024-12-19 08:25:50 -0600

    Overrides controller action to allow CORS.

---
+++ config/initializers/blacklight_iiif_search_controller_overrides.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# [Hyrax-overwrite-v3.4.2] sets 'Access-Control-Allow-Origin' before sending response.
+BlacklightIiifSearch::Controller.class_eval do
+  def iiif_search
+    headers['Access-Control-Allow-Origin'] = '*'
+    @parent_document = search_service.fetch(params[:solr_document_id])
+    iiif_search = IiifSearch.new(iiif_search_params, iiif_search_config, @parent_document)
+    iiif_search_service = search_service_class.new(config:      blacklight_config,
+                                                   user_params: iiif_search.solr_params)
+    @response = iiif_search_service.search_results
+    iiif_search_response = IiifSearchResponse.new(@response, @parent_document, self)
+
+    render json: iiif_search_response.annotation_list, content_type: 'application/json'
+  end
+end
