ad3c4519426d0d1397d8bf9d4b5cfc37199b8d2d
bwatson78 <brad.watson.orlando@gmail.com>
2024-12-19 08:42:42 -0600

    Adds parent modules to methods.

--- config/initializers/blacklight_iiif_search_controller_overrides.rb
+++ config/initializers/blacklight_iiif_search_controller_overrides.rb
@@ -5,11 +5,11 @@ BlacklightIiifSearch::Controller.class_eval do
   def iiif_search
     headers['Access-Control-Allow-Origin'] = '*'
     @parent_document = search_service.fetch(params[:solr_document_id])
-    iiif_search = IiifSearch.new(iiif_search_params, iiif_search_config, @parent_document)
+    iiif_search = BlacklightIiifSearch::IiifSearch.new(iiif_search_params, iiif_search_config, @parent_document)
     iiif_search_service = search_service_class.new(config:      blacklight_config,
                                                    user_params: iiif_search.solr_params)
     @response = iiif_search_service.search_results
-    iiif_search_response = IiifSearchResponse.new(@response, @parent_document, self)
+    iiif_search_response = BlacklightIiifSearch::IiifSearchResponse.new(@response, @parent_document, self)
 
     render json: iiif_search_response.annotation_list, content_type: 'application/json'
   end
