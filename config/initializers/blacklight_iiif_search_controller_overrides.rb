db6f178bcbbe190c49bd47c0f4aff63e027ea6eb
bwatson78 <brad.watson.orlando@gmail.com>
2024-12-19 10:32:46 -0600

    Revert override.

--- config/initializers/blacklight_iiif_search_controller_overrides.rb
+++ config/initializers/blacklight_iiif_search_controller_overrides.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 # [BlacklightIiifSearch-overwrite-v1.0.0] sets 'Access-Control-Allow-Origin' before sending response.
-BlacklightIiifSearch::Controller.class_eval do
-  # allow apps to load JSON received from a remote server
-  def set_access_headers
-    response.headers['Access-Control-Allow-Origin'] = 'https://digital-test.library.emory.edu'
-  end
-end
+# BlacklightIiifSearch::Controller.class_eval do
+#   # allow apps to load JSON received from a remote server
+#   def set_access_headers
+#     response.headers['Access-Control-Allow-Origin'] = 'https://digital-test.library.emory.edu'
+#   end
+# end
