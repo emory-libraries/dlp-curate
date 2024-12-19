04388a605896c38dad501a207233730c40cf63e8
bwatson78 <brad.watson.orlando@gmail.com>
2024-12-19 11:35:53 -0600

    enforce status code.

--- config/initializers/blacklight_iiif_search_controller_overrides.rb
+++ config/initializers/blacklight_iiif_search_controller_overrides.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 # [BlacklightIiifSearch-overwrite-v1.0.0] sets 'Access-Control-Allow-Origin' before sending response.
-# BlacklightIiifSearch::Controller.class_eval do
-#   # allow apps to load JSON received from a remote server
-#   def set_access_headers
-#     response.headers['Access-Control-Allow-Origin'] = 'https://digital-test.library.emory.edu'
-#   end
-# end
+BlacklightIiifSearch::Controller.class_eval do
+  def iiif_search
+    _parent_response, @parent_document = fetch(params[:solr_document_id])
+    iiif_search = BlacklightIiifSearch::IiifSearch.new(iiif_search_params, iiif_search_config, @parent_document)
+    @response, _document_list = search_results(iiif_search.solr_params)
+    iiif_search_response = BlacklightIiifSearch::IiifSearchResponse.new(@response, @parent_document, self)
+    render json:         iiif_search_response.annotation_list,
+           content_type: 'application/json',
+           status:       :ok
+  end
+end
