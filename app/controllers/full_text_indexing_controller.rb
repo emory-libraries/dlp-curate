736b0b8eeb95383fa564309afe0e6249f96d3cdf
bwatson78 <brad.watson.orlando@gmail.com>
2025-01-03 09:03:56 -0600

    Adds option to reindex child pages to work show page.

--- app/controllers/full_text_indexing_controller.rb
+++ app/controllers/full_text_indexing_controller.rb
@@ -4,7 +4,23 @@ class FullTextIndexingController < ApplicationController
   before_action :authenticate_user!
 
   def full_text_index
-    CompileFullTextJob.perform_later(work_id: params[:work_id], user_id: params[:user_id].to_i)
+    call_compiling_job
     redirect_to hyrax_curate_generic_work_path(params[:work_id]), notice: "Full-Text Indexing has been queued for this work."
   end
+
+  def full_text_index_with_pages
+    reindex_work_child_members
+    call_compiling_job
+    redirect_to hyrax_curate_generic_work_path(params[:work_id]), notice: "Full-Text Indexing has been queued for this work."
+  end
+
+  private
+
+    def call_compiling_job
+      CompileFullTextJob.perform_later(work_id: params[:work_id], user_id: params[:user_id].to_i)
+    end
+
+    def reindex_work_child_members
+      ReindexObjectChildrenJob.perform_later(params[:work_id])
+    end
 end
--- spec/routing/full_text_indexing_routing_spec.rb
+++ spec/routing/full_text_indexing_routing_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe "routes for full-text indexing", type: :routing do
         "user_id" => "1"
       )
   end
+
+  it "routes full-text indexing with pages requests to the full_text_indexing#full_text_index_with_pages" do
+    expect(post("/concern/curate_generic_works/1/full_text_index_with_pages?user_id=1"))
+      .to route_to(
+        "controller" => "full_text_indexing",
+        "action" => "full_text_index_with_pages",
+        "work_id" => "1",
+        "user_id" => "1"
+      )
+  end
 end
