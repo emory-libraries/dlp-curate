28121b3a7dd1aeebc06519afdbc34759b151b7e5
Brad Watson <brad.watson.orlando@gmail.com>
2025-04-15 07:39:37 -0500

    Changes deploy methods to the SelfDeposit process. (#2323)
    * Adds image height and width to Curate::TextExtraction::AltoReader for scaling.
    
    * Changes deploy methods to the SelfDeposit process.
    
    * remove the indexer code from this branch.

--- config/deploy/arch.rb
+++ config/deploy/arch.rb
@@ -3,12 +3,4 @@
 set :stage, :ARCH
 set :honeybadger_env, 'curate-arch'
 set :branding_symlink_path, '/mnt/arch_efs/uploads/dlp-curate/branding'
-
-ec2_role [:web, :app, :db, :redhatapp, :collection],
-         user:        'deploy',
-         ssh_options: {
-           keys:            ENV['SSH_EC2_KEY_FILE'],
-           forward_agent:   true,
-           verify_host_key: :never
-         }
-# server 'PRIVATE_IP_Address', user: 'deploy', roles: [:web, :app, :db, :redhatapp, :collection]
+server ENV['ARCH_SERVER_IP'], user: 'deploy', roles: [:web, :app, :db, :redhatapp, :collection]
