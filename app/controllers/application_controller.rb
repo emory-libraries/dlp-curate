24235a4222137d308be2cbf280cea984dc0c9125
bwatson78 <brad.watson.orlando@gmail.com>
2025-07-07 07:38:47 -0500

    Upgrades Curate to Hyrax v5.1.0.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   helper Zizia::Engine.helpers
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
-  skip_after_action :discard_flash_if_xhr
   include Hydra::Controller::ControllerBehavior
 
   # Adds Hyrax behaviors into the application controller
