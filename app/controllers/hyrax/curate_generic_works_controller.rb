c64d22189a87ce60955ae5534005da4ee587b3d0
bwatson78 <brad.watson.orlando@gmail.com>
2025-07-07 07:38:47 -0500

    Upgrades Curate to Hyrax v5.1.0.

--- app/controllers/hyrax/curate_generic_works_controller.rb
+++ app/controllers/hyrax/curate_generic_works_controller.rb
@@ -8,7 +8,12 @@ module Hyrax
     include Hyrax::WorksControllerBehavior
     include Hyrax::BreadcrumbsForWorks
 
-    self.curation_concern_type = ::CurateGenericWork
+    if Hyrax.config.valkyrie_transition?
+      self.curation_concern_type = ::GenericWorkResource
+      self.work_form_service = Hyrax::FormFactory.new
+    else
+      self.curation_concern_type = ::CurateGenericWork
+    end
 
     # Use this line if you want to use a custom presenter
     self.show_presenter = Hyrax::CurateGenericWorkPresenter
