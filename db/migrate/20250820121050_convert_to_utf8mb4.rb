3bb9e76d2b480cb275dd8d9fbeac9f89c862d76c
Brad Watson <brad.watson.orlando@gmail.com>
2025-08-28 09:31:21 -0500

    Converts Curate's DB to utf8mb4. (#2359)

---
+++ db/migrate/20250820121050_convert_to_utf8mb4.rb
@@ -0,0 +1,11 @@
+class ConvertToUtf8mb4 < ActiveRecord::Migration[5.2]
+  def change
+    # Change database encoding
+    execute "ALTER DATABASE `#{ActiveRecord::Base.connection.current_database}` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+
+    # Change table and column encoding
+    ActiveRecord::Base.connection.tables.each do |table_name|
+      execute "ALTER TABLE `#{table_name}` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+    end
+  end
+end
