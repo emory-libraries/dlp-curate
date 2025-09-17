b7f832263b222cfe2d0df902cb25559fb796ef2b
bwatson78 <brad.watson.orlando@gmail.com>
2025-09-17 12:30:28 -0500

    Adds PDFJS.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -7,3 +7,4 @@ Mime::Type.register "application/n-triples", :nt
 Mime::Type.register "application/ld+json", :jsonld
 Mime::Type.register "text/turtle", :ttl
 Mime::Type.register 'application/x-endnote-refer', :endnote
+Mime::Type.register "text/javascript", :mjs, %w( application/javascript application/x-javascript )
