19365039c9b20dfb630957b6b6d67790568b743d
Brad Watson <brad.watson.orlando@gmail.com>
2025-09-22 15:55:32 -0500

    Adds PDF Viewer and switch logic. (#2378)
    * Adds PDFJS.
    
    * change mime type registration.
    
    * restrict PDF viewer to Work level.
    
    * Eliminates locale setter.
    
    * appease the cop.

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -7,3 +7,4 @@ Mime::Type.register "application/n-triples", :nt
 Mime::Type.register "application/ld+json", :jsonld
 Mime::Type.register "text/turtle", :ttl
 Mime::Type.register 'application/x-endnote-refer', :endnote
+Rack::Mime::MIME_TYPES['.mjs'] = 'application/javascript'
