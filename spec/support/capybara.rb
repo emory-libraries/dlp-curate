fe5cef23ac2e6ed7d5307ef6c86c8928f2573f29
bwatson78 <brad.watson.orlando@gmail.com>
2025-11-14 16:21:59 -0600

    round 9.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,35 +1,5 @@
-<<<<<<< Updated upstream
 # frozen_string_literal: true
 
-Webdrivers.cache_time = 3
-
-# Setup chrome headless driver
-Capybara.server = :puma, { Silent: true }
-
-Capybara.register_driver :chrome_headless do |app|
-  client = Selenium::WebDriver::Remote::Http::Default.new
-  client.read_timeout = 120
-
-  options = ::Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('--headless')
-  options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
-  options.add_argument('--window-size=1400,1400')
-
-  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options, http_client: client)
-end
-
-Capybara.javascript_driver = :chrome_headless
-
-# Setup rspec
-RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :rack_test
-  end
-
-  config.before(:each, type: :system, js: true) do
-    driven_by :chrome_headless
-=======
 # without lines 1-12, screenshots and html captured from failing specs are blank
 # source: https://github.com/mattheworiordan/capybara-screenshot/issues/225
 require "action_dispatch/system_testing/test_helpers/setup_and_teardown"
@@ -123,6 +93,5 @@ RSpec.configure do |config|
     # Quitting forces the browser session to be reinitialized during the next :js spec.
     # This is slower but more resilient to timeouts (in theory).
     Capybara.page.driver.quit
->>>>>>> Stashed changes
   end
 end
