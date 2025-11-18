34060f826b40317a5d19965abf36b69a5b2a764f
bwatson78 <brad.watson.orlando@gmail.com>
2025-11-18 08:53:21 -0600

    round 14.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,4 +5,4 @@ include Ec2Ipv4Retriever
 set :stage, :PROD
 set :honeybadger_env, 'curate'
 set :branding_symlink_path, '/mnt/prod_efs/uploads/dlp-curate/branding'
-server find_ip_by_ec2_name(ec2_name: 'curate-prod.library.emory.edu') || ENV['PROD_SERVER_IP'], user: 'deploy', roles: [:web, :app, :db, :redhatapp, :collection]
+server find_ip_by_ec2_name(ec2_name: 'curate.library.emory.edu') || ENV['PROD_SERVER_IP'], user: 'deploy', roles: [:web, :app, :db, :redhatapp, :collection]
