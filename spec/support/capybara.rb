1d5b3055846a39b3ae5d951fa2289300e85517f4
bwatson78 <brad.watson.orlando@gmail.com>
2025-11-24 07:40:11 -0600

    round 15.

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -21,10 +21,8 @@ require 'selenium-webdriver'
 Capybara.save_path = ENV['CI'] ? "/tmp/test-results" : Rails.root.join('tmp', 'capybara')
 
 options = Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-  opts.add_argument("--headless=new") if ENV["CHROME_HEADLESS_MODE"]
-  # opts.add_argument("--no-sandbox")
-  # opts.add_argument("--disable-dev-shm-usage")
-  opts.add_argument("--disable-gpu") if Gem.win_platform?
+  opts.add_argument("--headless=new")
+  opts.add_argument("--disable-gpu")
   opts.add_argument("--window-size=1440,1440")
   # opts.add_argument("--enable-features=NetworkService,NetworkServiceInProcess")
   # opts.add_argument("--disable-features=VizDisplayCompositor")
@@ -33,7 +31,7 @@ end
 Capybara.register_driver :selenium_chrome_headless_sandboxless do |app|
   driver = Capybara::Selenium::Driver.new(app,
                                       browser: :remote,
-                                      capabilities: options,
+                                      options: options,
                                       url: ENV['HUB_URL'])
 
   # Fix for capybara vs remote files. Selenium handles this for us
@@ -47,22 +45,21 @@ end
 
 Capybara.server_host = '0.0.0.0'
 Capybara.server_port = 3010
+Capybara.always_include_port = true
 
-# ip = IPSocket.getaddress(Socket.gethostname)
-ip = `hostname -s`.strip
+ip = IPSocket.getaddress(Socket.gethostname)
 Capybara.app_host = "http://#{ip}:#{Capybara.server_port}"
 
 Capybara.default_driver = :rack_test # This is a faster driver
 Capybara.javascript_driver = :selenium_chrome_headless_sandboxless # This is slower
-Capybara.disable_animation = true
 Capybara.default_max_wait_time = ENV.fetch('CAPYBARA_WAIT_TIME', 10) # We may have a slow application, let's give it some time.
 
 Capybara::Screenshot.register_driver(:selenium_chrome_headless_sandboxless) do |driver, path|
   driver.browser.save_screenshot(path)
 end
 
-Capybara::Screenshot.autosave_on_failure = true
-Capybara::Screenshot.prune_strategy = :keep_last_run
+Capybara::Screenshot.autosave_on_failure = false
+Capybara::Screenshot.prune_strategy = { keep: 10 }
 
 # Save CircleCI artifacts
 
@@ -73,7 +70,7 @@ def save_timestamped_page_and_screenshot(page, meta)
   time_now = Time.zone.now
   timestamp = "#{time_now.strftime('%Y-%m-%d-%H-%M-%S.')}#{'%03d' % (time_now.usec / 1000).to_i}"
 
-  artifact_dir = Capybara.save_path
+  artifact_dir = ENV['CI'] ? "/tmp/test-results" : Rails.root.join('tmp', 'capybara')
 
   screenshot_name = "screenshot-#{filename}-#{line_number}-#{timestamp}.png"
   screenshot_path = "#{artifact_dir}/#{screenshot_name}"
@@ -90,8 +87,5 @@ end
 RSpec.configure do |config|
   config.after(:each, :js) do |example|
     save_timestamped_page_and_screenshot(Capybara.page, example.metadata) if example.exception
-    # Quitting forces the browser session to be reinitialized during the next :js spec.
-    # This is slower but more resilient to timeouts (in theory).
-    Capybara.page.driver.quit
   end
 end
