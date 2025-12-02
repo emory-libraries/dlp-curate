35f5ff6f00e86250167087b5ce88c6349ed3bb3b
bwatson78 <brad.watson.orlando@gmail.com>
2025-12-02 16:01:05 -0600

    batch 8.

--- config/prepends/custom_permission_badge.rb
+++ config/prepends/custom_permission_badge.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
+# [Hyrax-overwrite-v5.2.0] We do not want to display the institution name in the Authenticated permission badge
 
 module CustomPermissionBadge
   private
 
-    # [Hyrax-overwrite-v5.2.0] We do not want to display the
-    # institution name in the Authenticated permission badge
     def text
       visibility_key = @visibility || 'unknown'
       I18n.t("hyrax.visibility.#{visibility_key}.text")
