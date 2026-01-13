3fe756289be4df80a86fa3c97ddc799854d72e50
bwatson78 <brad.watson.orlando@gmail.com>
2026-01-13 09:16:01 -0600

    Makes the importers Bulkrax-only.

--- app/importers/langmuir_preprocessor.rb
+++ app/importers/langmuir_preprocessor.rb
@@ -5,10 +5,6 @@ require 'ruby-progressbar'
 ##
 # Utility service and methods that merge metadata from a CSV Pull List and MARCXml records
 # into a format suitable for ingest by the curate CSV importer
-# Deprecation Warning: As of Curate v3, Zizia will be removed. This preprocessor contains
-#   custom logic that switches expected output based on whether Bulkrax is or isn't the importer.
-#   This should be refactored to only export Bulkrax expected fields when Zizia is removed.
-
 class LangmuirPreprocessor
   attr_accessor :processed_csv
 
@@ -17,10 +13,10 @@ class LangmuirPreprocessor
   # @param [String] csv the path to a CSV file containing the expectd Pull List metadata
   # Change below was necessary to institute Source/Deposit Collection structure.
   # For more information, read the SOURCE_DEPOSIT_CHANGES_README.md in dlp-curate's root folder.
-  def initialize(csv, importer)
+  def initialize(csv)
     @source_csv = CSV.read(csv, headers: true)
-    @is_for_bulkrax = importer == 'bulkrax'
-    @fileset_model_or_type = @is_for_bulkrax ? 'FileSet' : 'fileset'
+    @fileset_model_or_type = 'FileSet'
+    @work_model_or_type = 'CurateGenericWork'
     directory = File.dirname(csv)
     extension = File.extname(csv)
     filename = File.basename(csv, extension)
@@ -34,9 +30,8 @@ class LangmuirPreprocessor
   end
 
   def additional_headers
-    ['source_row', 'deduplication_key', @is_for_bulkrax ? 'model' : 'type', 'parent',
-     'file', 'file_types', 'pcdm_use', 'fileset_label', 'preservation_master_file',
-     'intermediate_file']
+    ['source_row', 'deduplication_key', 'model', 'parent', 'file', 'file_types', 'pcdm_use', 'fileset_label',
+     'preservation_master_file', 'intermediate_file']
   end
 
   # process_source_rows builds
@@ -59,6 +54,7 @@ class LangmuirPreprocessor
 
   def process_source_rows
     progressbar = ProgressBar.create(title: "Processing Source", total: @source_csv.size, format: '%t: |%B| %p%   ')
+
     @source_csv.each.with_index do |row, row_num|
       process_row(row, row_num + 2) if row['Digital Object - Parent Identifier'] # skip blank rows in the source csv
       progressbar.increment
@@ -67,8 +63,11 @@ class LangmuirPreprocessor
 
   def output_work_tree
     merge_csv = CSV.open(@processed_csv, 'w+', headers: true, write_headers: true)
+
     merge_csv << @merged_headers
+
     progressbar = ProgressBar.create(title: "Writing works", total: @tree.size, format: '%t: |%B| %p%   ')
+
     @tree.each_value do |work|
       merge_csv << work[:metadata]
       two_sided = work[:filesets].count <= 2
@@ -83,14 +82,14 @@ class LangmuirPreprocessor
       fileset = work[:filesets][fileset_index]
 
       process_fileset_title_label(fileset, fileset_index, two_sided)
-      process_fileset_file_and_file_type(fileset) if @is_for_bulkrax
+      process_fileset_file_and_file_type(fileset)
       merge_csv << fileset
     end
   end
 
   def process_fileset_title_label(fileset, fileset_index, two_sided)
     fileset_label = make_label(fileset_index, two_sided)
-    @is_for_bulkrax ? fileset['title'] = fileset_label : fileset['fileset_label'] = fileset_label
+    fileset['title'] = fileset_label
   end
 
   def process_fileset_file_and_file_type(fileset)
@@ -98,14 +97,13 @@ class LangmuirPreprocessor
     int_filename = fileset['intermediate_file']&.split('/')&.last
     pres_file_type_chunk = "#{pres_filename}:preservation_master_file" if pres_filename.present?
     int_file_type_chunk = "#{int_filename}:intermediate_file" if int_filename.present?
-
     fileset['file'] = [pres_filename, int_filename].compact.join(';')
     fileset['file_types'] = [pres_file_type_chunk, int_file_type_chunk].compact.join('|')
   end
 
   def process_row(row, source_row_num)
     parent = row['Digital Object - Parent Identifier']
-    deduplication_key = @is_for_bulkrax ? '' : parent
+    deduplication_key = ''
     @sequence_number, @target_file, @metadata_row = extract_structure(row)
     @tree[parent] ||= { metadata: nil, filesets: {} } # create a placeholder if we don't have one for this key
 
@@ -129,20 +127,22 @@ class LangmuirPreprocessor
     p_number = filename.scan(/P0+(\d+)_(ARCH|PROD)/)[0][0].to_i
     target_file = filename.include?('ARCH') ? 'preservation_master_file' : 'intermediate_file'
     metadata_row = p_number == 1 && target_file == 'preservation_master_file'
+
     [p_number, target_file, metadata_row]
   end
 
   def extract_metadata(row, source_row_num)
     parent = row['source_collection_id']
     deduplication_key = row['Digital Object - Parent Identifier']
-    model_or_type = @is_for_bulkrax ? 'CurateGenericWork' : 'work'
-    processed_row = CSV::Row.new(additional_headers, [source_row_num, deduplication_key, model_or_type, parent, '', '', ''])
+    processed_row = CSV::Row.new(additional_headers, [source_row_num, deduplication_key, @work_model_or_type, parent, '', '', ''])
+
     processed_row << row.to_hash
   end
 
   def relative_path_to_file(row)
     absolute_path = row['Path'] # e.g. "::nasn2dmz.cc.emory.edu:dmfiles:MARBL:Manuscripts:MSS_1218_Langmuir:ARCH:B071:MSS1218_B071_I205_P0001_ARCH.tif"
     relative_path = absolute_path.gsub(/::(\w+\.)*\w+:/, '') # remove any initial ::hostname.domain: prefix
+
     relative_path.tr(':', '/') # convert path segment separators from colons to forward slashes
   end
 
