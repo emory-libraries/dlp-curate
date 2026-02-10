7d90aec4f8c3a8bac86471e0530af495b530adff
bwatson78 <brad.watson.orlando@gmail.com>
2026-02-10 13:09:23 -0600

    rspec batch 1

--- app/presenters/hyrax/admin_set_presenter.rb
+++ app/presenters/hyrax/admin_set_presenter.rb
@@ -22,7 +22,7 @@ module Hyrax
       default_set? || any_items?
     end
 
-    # Message to display if deletion is disabled
+     # Message to display if deletion is disabled
     def disabled_message
       return I18n.t('hyrax.admin.admin_sets.delete.error_default_set') if default_set?
       I18n.t('hyrax.admin.admin_sets.delete.error_not_empty') if any_items?
