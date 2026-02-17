8de1b69a8d0fc6e7593ab41a2f450d5d7f54f4fe
bwatson78 <brad.watson.orlando@gmail.com>
2026-02-17 16:42:26 -0600

    Fixes spec/system/admin_dashboard_spec.rb.

--- spec/system/admin_dashboard_spec.rb
+++ spec/system/admin_dashboard_spec.rb
@@ -3,71 +3,45 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.describe 'Admin dashboard', integration: true, clean: true, type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  before do
+    login_as user
+    visit '/dashboard'
+  end
+
   context 'as a non-admin user' do
-    let(:user) { FactoryBot.create(:user) }
-    before do
-      login_as user
-      visit '/dashboard'
-    end
+    it('does not have settings tab') { expect(page).not_to have_link 'Settings' }
 
-    scenario 'does not have settings tab' do
-      expect(page).not_to have_link 'Settings'
-    end
+    it('does not have import csv link') { expect(page).not_to have_link 'Import Content From a CSV' }
 
-    scenario 'does not have import csv link' do
-      expect(page).not_to have_link 'Import Content From a CSV'
-    end
+    it('does not have Bulkrax Importers link') { expect(page).not_to have_link 'Importers' }
 
-    scenario 'does not have Bulkrax Importers link' do
-      expect(page).not_to have_link 'Importers'
-    end
-
-    scenario 'does not have Bulkrax Exporters link' do
-      expect(page).not_to have_link 'Exporters'
-    end
+    it('does not have Bulkrax Exporters link') { expect(page).not_to have_link 'Exporters' }
   end
 
   context 'as an admin user' do
-    let(:admin) { FactoryBot.create(:admin) }
-    before do
-      login_as admin
-      visit '/dashboard'
-    end
+    let(:user) { FactoryBot.create(:admin) }
 
-    scenario 'view the workflow roles page' do
+    it('does have settings tab') { expect(page).to have_link 'Settings' }
+
+    it('does have background jobs tab') { expect(page).to have_link 'Background Jobs' }
+
+    it('does have Bulkrax Importers link') { expect(page).to have_link 'Importers' }
+
+    it('does have Bulkrax Exporters link') { expect(page).to have_link 'Exporters' }
+
+    it 'provides the workflow roles page' do
       click_on 'Workflow Roles'
       expect(page).to have_content 'Assign Role'
     end
 
-    scenario 'does have settings tab' do
-      expect(page).to have_link 'Settings'
-    end
-
-    scenario 'does  have an import csv link' do
-      expect(page).to have_link 'Import Content From a CSV'
-    end
-
-    scenario 'does have background jobs tab' do
-      expect(page).to have_link 'Background Jobs'
-    end
-
-    scenario 'view background jobs page' do
+    it 'provides the background jobs page' do
       click_on 'Background Jobs'
       expect(page).to have_content 'Select Job'
     end
 
-    scenario 'does have Bulkrax Importers link' do
-      expect(page).to have_link 'Importers'
+    it 'does not have an import csv link in Hyrax v5.2.0 (Bulkrax is now the only game)' do
+      expect(page).not_to have_link 'Import Content From a CSV'
     end
-
-    scenario 'does have Bulkrax Exporters link' do
-      expect(page).to have_link 'Exporters'
-    end
-    # TODO: Add more admin tests, for eg: stats page, when work is created
-
-    # scenario 'view the statistics page' do
-    #   click_on 'Reports'
-    #   expect(page).to have_content ''
-    # end
   end
 end
