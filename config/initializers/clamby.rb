1f3ad1079d94098585c12530e52c233d6c94ec71
bwatson78 <brad.watson.orlando@gmail.com>
2026-03-13 07:31:05 -0500

    Adds reloader to clamby initializer.

--- config/initializers/clamby.rb
+++ config/initializers/clamby.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
+Rails.application.reloader.to_prepare do
+  Hydra::Works.default_system_virus_scanner = ::ClambyScanner
 
-Hydra::Works.default_system_virus_scanner = ::ClambyScanner
-
-if Rails.env.production?
-  Clamby.configure(
-    check:                  false, # only used for development environment
-    daemonize:              true,
-    error_clamscan_missing: true,
-    error_file_missing:     true,
-    error_file_virus:       true,
-    fdpass:                 true
-  )
+  if Rails.env.production?
+    Clamby.configure(
+      check:                  false, # only used for development environment
+      daemonize:              true,
+      error_clamscan_missing: true,
+      error_file_missing:     true,
+      error_file_virus:       true,
+      fdpass:                 true
+    )
+  end
 end
