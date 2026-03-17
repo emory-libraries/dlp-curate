0382ec623b4a558c41af3ddbe4d6558e81473678
bwatson78 <brad.watson.orlando@gmail.com>
2026-03-17 15:04:34 -0500

    Swap to dart sass.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Rails.application.reloader.to_prepare do
+Rails.application.config.to_prepare do
   Riiif::Image.info_service = lambda do |id, _file|
     # id will look like a path to a pcdm:file
     # (e.g. rv042t299%2Ffiles%2F6d71677a-4f80-42f1-ae58-ed1063fd79c7)
@@ -36,7 +36,7 @@ end
 module Hyrax
   # Adds file locking to Riiif::File
   # @see RiiifFileResolver
-  Rails.application.reloader.to_prepare do
+  Rails.application.config.to_prepare do
     class RiiifFile < Riiif::File
       include ActiveSupport::Benchmarkable
 
--- config/initializers/works_controller.rb
+++ config/initializers/works_controller.rb
@@ -4,12 +4,14 @@
 #
 #   For more information, read the SOURCE_DEPOSIT_CHANGES_README.md in dlp-curate's root folder.
 
-Hyrax::My::WorksController.class_eval do
-  # Define collection specific filter facets.
-  def self.configure_facets
-    configure_blacklight do |config|
-      config.add_facet_field "source_collection_title_ssim", limit: 5, label: 'Source Collection'
+Rails.application.config.to_prepare do
+  Hyrax::My::WorksController.class_eval do
+    # Define collection specific filter facets.
+    def self.configure_facets
+      configure_blacklight do |config|
+        config.add_facet_field "source_collection_title_ssim", limit: 5, label: 'Source Collection'
+      end
     end
+    configure_facets
   end
-  configure_facets
 end
