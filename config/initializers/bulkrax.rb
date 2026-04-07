69a20594f2c870cfc3c4531be45d841cd9265fd6
bwatson78 <brad.watson.orlando@gmail.com>
2026-04-07 10:14:19 -0500

    Get bulkrax importer show page datatables loading.

--- config/initializers/bulkrax.rb
+++ config/initializers/bulkrax.rb
@@ -542,9 +542,9 @@ Rails.application.config.to_prepare do
     def curate_obj_text(entry)
       obj = entry.importerexporter_type == "Bulkrax::Exporter" ? entry&.hyrax_record : entry&.factory&.find
       text_array = []
-      link = if defined?(Hyrax) && entry.factory_class.model_name.human == 'Collection'
+      link = if defined?(Hyrax) && entry.factory_class.model_name.human == 'Collection' && obj.present?
                hyrax.polymorphic_path(obj)
-             else
+             elsif obj.present?
                main_app.polymorphic_path(obj)
              end
 
