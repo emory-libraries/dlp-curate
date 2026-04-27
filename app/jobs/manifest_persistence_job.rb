4e8c99d76dd461e841bdf01a74542aca62a0f700
Alex Zotov <alexandr.zotov@emory.edu>
2026-04-27 12:38:19 -0500

    jobs and characterization integration

--- app/jobs/manifest_persistence_job.rb
+++ app/jobs/manifest_persistence_job.rb
@@ -41,21 +41,32 @@ class ManifestPersistenceJob < Hyrax::ApplicationJob
     end
 
     def image_concerns(curation_concern)
-      check_for_nil_in_ordered_members(curation_concern:)
-      file_set_ids = pulled_ordered_members(curation_concern:)&.compact
-      if file_set_ids.empty?
-        []
+      file_set_ids = file_set_member_ids(curation_concern).compact
+      file_set_ids.presence || []
+    end
+
+    def file_set_member_ids(curation_concern)
+      case curation_concern
+      when Hyrax::Resource
+        valkyrie_file_set_ids(curation_concern)
       else
-        file_set_ids
+        af_file_set_ids(curation_concern)
       end
     end
 
-    def pulled_ordered_members(curation_concern:)
-      curation_concern.ordered_member_ids - curation_concern.child_work_ids
+    def af_file_set_ids(curation_concern)
+      ids = curation_concern.ordered_member_ids
+      log_nil_members(curation_concern) if ids.any?(nil)
+      ids - curation_concern.child_work_ids
     end
 
-    def check_for_nil_in_ordered_members(curation_concern:)
-      return unless pulled_ordered_members(curation_concern:).any?(nil)
+    def valkyrie_file_set_ids(curation_concern)
+      Hyrax.custom_queries
+           .find_child_file_sets(resource: curation_concern)
+           .map { |fs| fs.id.to_s }
+    end
+
+    def log_nil_members(curation_concern)
       Rails.logger.error "The CurateGenericWork with the id #{curation_concern.id} contains nil objects in its ordered_members."
     end
 end
