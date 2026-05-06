bc5597f702fef79f905510f83554bc8a48869672
azotov <alexandr.zotov@emory.edu>
2026-05-06 07:46:43 -0500

    background jobs integrations (#2525)

--- app/jobs/collection_files_ingested_job.rb
+++ app/jobs/collection_files_ingested_job.rb
@@ -20,7 +20,7 @@ class CollectionFilesIngestedJob < Hyrax::ApplicationJob
   private
 
     def selected_coll_hsh(collection_array)
-      collections = collection_array.map { |coll| Collection.find(coll) }
+      collections = collection_array.map { |coll| find_collection(coll) }
       collections_hasherizer(collections)
     end
 
@@ -53,14 +53,22 @@ class CollectionFilesIngestedJob < Hyrax::ApplicationJob
       filesets.sum { |fs| fs['sha1_tesim']&.compact&.uniq&.size || 0 }
     end
 
+    def find_collection(id)
+      if Hyrax.config.valkyrie_transition?
+        Hyrax.query_service.find_by(id:)
+      else
+        Collection.find(id)
+      end
+    end
+
     # Change below was necessary to institute Source/Deposit Collection structure.
     # For more information, read the SOURCE_DEPOSIT_CHANGES_README.md in dlp-curate's root folder.
     def source_collections
-      # The determination of which collections are considered Source Collections
-      # is based solely on the Collection's CollectionType deposit_only_collection
-      # attribute. This logic can be changed, but the simplest determination is the
-      # CollectionType.
-      Collection.all.select { |c| !Hyrax::CollectionType&.for(collection: c)&.deposit_only_collection? }
+      collection_docs = Hyrax::SolrService.query("has_model_ssim:Collection", rows: 1_000_000, fl: "id")
+      collection_docs.filter_map do |doc|
+        col = find_collection(doc["id"])
+        col unless Hyrax::CollectionType&.for(collection: col)&.deposit_only_collection?
+      end
     end
 
     def collections_hasherizer(collection_array)
