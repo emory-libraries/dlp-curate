02d23d99e923fbc66bdfc8a9423b5eff0544f7bd
Alex Zotov <alexandr.zotov@emory.edu>
2026-05-06 07:44:59 -0500

    background jobs integrations

--- app/jobs/transfer_works_job.rb
+++ app/jobs/transfer_works_job.rb
@@ -4,6 +4,17 @@ require 'stackprof'
 class TransferWorksJob < Hyrax::ApplicationJob
   def perform(import_col_id, true_col_id)
     ::StackProf.run(mode: :cpu, out: 'tmp/stackprof-curate.dump', raw: true, ignore_gc: true) do
+      if Hyrax.config.valkyrie_transition?
+        transfer_valkyrie(import_col_id, true_col_id)
+      else
+        transfer_af(import_col_id, true_col_id)
+      end
+    end
+  end
+
+  private
+
+    def transfer_af(import_col_id, true_col_id)
       import_col = Collection.find(import_col_id)
       true_col = Collection.find(true_col_id)
       import_col.member_works.each do |work|
@@ -12,5 +23,26 @@ class TransferWorksJob < Hyrax::ApplicationJob
         work.save!
       end
     end
-  end
+
+    def transfer_valkyrie(import_col_id, true_col_id)
+      work_docs = Hyrax::SolrService.query(
+        "member_of_collection_ids_ssim:#{import_col_id} AND has_model_ssim:CurateGenericWork",
+        rows: 1_000_000, fl: "id"
+      )
+
+      work_docs.each do |doc|
+        transfer_valkyrie_work(doc["id"], import_col_id, true_col_id)
+      end
+    end
+
+    def transfer_valkyrie_work(work_id, import_col_id, true_col_id)
+      work = Hyrax.query_service.find_by(id: work_id)
+      col_ids = Array(work.member_of_collection_ids).map { |cid| Valkyrie::ID.new(cid.to_s) }
+      true_col_valkyrie_id = Valkyrie::ID.new(true_col_id)
+      col_ids << true_col_valkyrie_id unless col_ids.any? { |cid| cid.to_s == true_col_id }
+      col_ids.reject! { |cid| cid.to_s == import_col_id }
+      work.member_of_collection_ids = col_ids
+      Hyrax.persister.save(resource: work)
+      Hyrax.index_adapter.save(resource: work)
+    end
 end
--- app/jobs/work_members_clean_up_job.rb
+++ app/jobs/work_members_clean_up_job.rb
@@ -9,25 +9,35 @@ class WorkMembersCleanUpJob < Hyrax::ApplicationJob
     works.each { |work| clean_up_work_member(work) }
   end
 
-  def pull_works_by_ids(work_ids)
-    ret_arr = []
-    work_ids.each do |id|
-      ret_arr << CurateGenericWork.find(id)
-    rescue
-      Rails.logger.error "The id #{id} did not match a work in the system."
-      next
-    end
-    ret_arr
-  end
+  private
 
-  def clean_up_work_member(work)
-    members_count = work.ordered_member_ids.size
-    cleaned_member_ids = work.ordered_member_ids.compact.uniq
-
-    if members_count != cleaned_member_ids.size
-      ReplaceWorkMembersJob.perform_later(work, cleaned_member_ids)
-    else
-      Rails.logger.error 'No nil or duplicate work members were found.'
+    def pull_works_by_ids(work_ids)
+      ret_arr = []
+      work_ids.each do |id|
+        ret_arr << find_work(id)
+      rescue ActiveFedora::ObjectNotFoundError, Valkyrie::Persistence::ObjectNotFoundError, StandardError
+        Rails.logger.error "The id #{id} did not match a work in the system."
+        next
+      end
+      ret_arr
     end
-  end
+
+    def find_work(id)
+      if Hyrax.config.valkyrie_transition?
+        Hyrax.query_service.find_by(id:)
+      else
+        CurateGenericWork.find(id)
+      end
+    end
+
+    def clean_up_work_member(work)
+      member_ids = work.member_ids
+      cleaned_member_ids = member_ids.compact.uniq
+
+      if member_ids.size != cleaned_member_ids.size
+        ReplaceWorkMembersJob.perform_later(work, cleaned_member_ids.map(&:to_s))
+      else
+        Rails.logger.error 'No nil or duplicate work members were found.'
+      end
+    end
 end
