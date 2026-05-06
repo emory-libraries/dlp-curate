a0c1e73a7ae5fc963b8d27eaea50a3e7e048bc05
azotov <alexandr.zotov@emory.edu>
2026-05-06 08:03:51 -0500

    views and presenters integrations (#2526)

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -4,6 +4,7 @@ module FormHelper
   # Change below was necessary to institute Source/Deposit Collection structure.
   # For more information, read the SOURCE_DEPOSIT_CHANGES_README.md in dlp-curate's root folder.
   def all_collections_collection
-    Collection.all.map { |c| [c.title.first, c.id] }
+    results = Hyrax::SolrService.query("has_model_ssim:Collection", rows: 1_000_000, fl: "id,title_tesim")
+    results.map { |doc| [Array(doc["title_tesim"]).first, doc["id"]] }
   end
 end
