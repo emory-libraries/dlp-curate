f1422b105640d93dfeb0c0a8e9bc43ad9abd1356
bwatson78 <brad.watson.orlando@gmail.com>
2026-06-18 12:35:15 -0500

    just pass arguments.

--- app/jobs/create_preservation_event_job.rb
+++ app/jobs/create_preservation_event_job.rb
@@ -13,7 +13,7 @@ class CreatePreservationEventJob < Hyrax::ApplicationJob
 
   def report_completely_failed_creation(job:, exception:)
     CSV.open(file_path, "a+", write_headers: !csv_exists?, headers: pres_event_headers) do |csv|
-      row = [job.job_id, exception.message, job.arguments[:object].id, event['details'], event['end'], event['start'],
+      row = [job.job_id, exception.message, job.arguments, event['details'], event['end'], event['start'],
              event['type'], event['user'], event['outcome'], event['software_version']]
 
       csv << row
@@ -31,7 +31,7 @@ class CreatePreservationEventJob < Hyrax::ApplicationJob
     end
 
     def pres_event_headers
-      ['Job ID', 'Exception', 'Object ID', 'event_details', 'event_end', 'event_start',
+      ['Job ID', 'Exception', 'Arguments', 'event_details', 'event_end', 'event_start',
        'event_type', 'initiating_user', 'outcome', 'software_version']
     end
 end
