7e7f21f85ea5eeefb9dab711fdcca677ab3bde51
bwatson78 <brad.watson.orlando@gmail.com>
2026-06-18 11:28:13 -0500

    Moves report_completely_failed_creation method out of private.

--- app/jobs/create_preservation_event_job.rb
+++ app/jobs/create_preservation_event_job.rb
@@ -4,11 +4,20 @@ class CreatePreservationEventJob < Hyrax::ApplicationJob
   include PreservationEvents
 
   retry_on(Exception) do |job, exception|
-    report_completely_failed_creation(job:, exception:) if exception&.message&.present?
+    job.report_completely_failed_creation(job:, exception:) if exception&.message&.present?
   end
 
   def perform(object:, event:)
     create_preservation_event(object, event)
+  end
+
+  def report_completely_failed_creation(job:, exception:)
+    CSV.open(file_path, "a+", write_headers: !csv_exists?, headers: pres_event_headers) do |csv|
+      row = [job.id, exception.message, object.id, event['details'], event['end'], event['start'],
+             event['type'], event['user'], event['outcome'], event['software_version']]
+
+      csv << row
+    end
   end
 
   private
@@ -24,14 +33,5 @@ class CreatePreservationEventJob < Hyrax::ApplicationJob
     def pres_event_headers
       ['Job ID', 'Exception', 'Object ID', 'event_details', 'event_end', 'event_start',
        'event_type', 'initiating_user', 'outcome', 'software_version']
-    end
-
-    def report_completely_failed_creation(job:, exception:)
-      CSV.open(file_path, "a+", write_headers: !csv_exists?, headers: pres_event_headers) do |csv|
-        row = [job.id, exception.message, object.id, event['details'], event['end'], event['start'],
-               event['type'], event['user'], event['outcome'], event['software_version']]
-
-        csv << row
-      end
     end
 end
