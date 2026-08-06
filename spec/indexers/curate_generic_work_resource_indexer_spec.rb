ce0617eb6e14e2f4e359243c9aacbdf00a962876
Alex Zotov <alexandr.zotov@emory.edu>
2026-08-06 09:49:15 -0500

    admin unauthorized private work

--- spec/indexers/curate_generic_work_resource_indexer_spec.rb
+++ spec/indexers/curate_generic_work_resource_indexer_spec.rb
@@ -27,6 +27,10 @@ RSpec.describe CurateGenericWorkResourceIndexer do
     it 'returns a hash' do
       expect(solr_document).to be_a(Hash)
     end
+
+    it 'indexes has_model_ssim as the AF model name for search compatibility' do
+      expect(solr_document['has_model_ssim']).to eq 'CurateGenericWork'
+    end
   end
 
   describe 'sort and date fields' do
