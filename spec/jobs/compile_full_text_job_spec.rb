c7dcc6d7537527196fe77be3f657680ce0396fa8
Alex Zotov <alexandr.zotov@emory.edu>
2026-09-01 15:04:23 -0500

    fixes rspec 3

--- spec/jobs/compile_full_text_job_spec.rb
+++ spec/jobs/compile_full_text_job_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'rails_helper'
 
-RSpec.describe CompileFullTextJob, :clean, perform_enqueued: [AttachFilesToWorkJob, IngestJob] do
+RSpec.describe CompileFullTextJob, :clean, perform_enqueued: [AttachFilesToWorkJob, CurateAfIngestJob] do
   let(:uploaded_file_1) do
     FactoryBot.build(:uploaded_file,
                      file:                     'Page 1',
