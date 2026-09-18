0488f9a45775debdaddaec6434653d1098baaa0d
Brad Watson <brad.watson.orlando@gmail.com>
2026-09-18 14:06:04 -0500

    Reworks the AssociateFilesetsWithWorksJob. (#2700)

--- app/lib/associate_filesets_with_works.rb
+++ app/lib/associate_filesets_with_works.rb
@@ -3,7 +3,7 @@
 # This module will be used to define methods that will associate filesets with works
 module AssociateFilesetsWithWorks
   def pull_file_set_entries(importer:)
-    entries_to_return = importer.entries.select { |e| e.factory_class == FileSet }
+    entries_to_return = importer.entries.select { |e| e.factory_class == FileSet || e.factory_class == FileSetResource }
     raise 'The are no FileSet entries to iterate over' if entries_to_return.blank?
     entries_to_return
   end
@@ -36,46 +36,47 @@ module AssociateFilesetsWithWorks
     Hyrax.query_service.find_by(id: results.first["id"])
   end
 
-  def pull_fileset_entries_for_parent(file_set_entries, parent)
-    file_set_entries.select { |fse| fse.parsed_metadata['parent'] == [parent] }
+  def pull_fileset_entries_for_parent(parent)
+    @file_set_entries.select { |fse| fse.parsed_metadata['parent'] == [parent] }
   end
 
-  def pull_file_sets(file_set_entries, parent)
-    pull_fileset_entries_for_parent(file_set_entries, parent).map { |v| v&.factory&.find&.presence }.compact.uniq
+  def pull_file_sets(parent)
+    pull_fileset_entries_for_parent(parent).map { |v| v&.factory&.find&.presence }.compact.uniq
   end
 
   def process_file_sets(parents:, file_set_entries:)
+    @file_set_entries = file_set_entries
+
     parents.each do |p|
       work = pull_work(p)
-      file_sets = pull_file_sets(file_set_entries, p)
+      file_sets = pull_file_sets(p)
       raise 'A CurateGenericWork and/or FileSet objects could not be found' unless work.present? && file_sets.present?
 
-      associate_filesets_to_work(file_sets, work)
+      associate_filesets_to_work(work, file_sets)
       announce_filesets_attachement(file_sets)
     end
   end
 
-  def associate_filesets_to_work(file_sets, work)
+  def associate_filesets_to_work(work, file_sets)
     case work
     when Hyrax::Resource
-      associate_valkyrie(file_sets, work)
+      associate_valkyrie(work, file_sets)
     else
-      associate_af(file_sets, work)
+      associate_af(work, file_sets)
     end
   end
 
-  def associate_af(file_sets, work)
+  def associate_af(work, file_sets)
     return if file_sets&.map(&:id)&.all? { |id| work.reload.ordered_member_ids.include?(id) }
+
     work.ordered_members += file_sets
     work.save
   end
 
-  def associate_valkyrie(file_sets, work)
-    existing_ids = work.member_ids.map(&:to_s)
-    new_ids = file_sets.map { |fs| fs.id.to_s } - existing_ids
-    return if new_ids.empty?
+  def associate_valkyrie(work, file_sets)
+    raise "All FileSetResources haven't been persisted yet" unless pull_fileset_entries_for_parent(work.deduplication_key).size == file_sets.size
 
-    work.member_ids += new_ids.map { |id| Valkyrie::ID.new(id) }
+    work.member_ids = file_sets.map(&:id)
     Hyrax.persister.save(resource: work)
     Hyrax.index_adapter.save(resource: work)
   end
@@ -84,7 +85,7 @@ module AssociateFilesetsWithWorks
     file_sets.each { |fs| Hyrax.config.callback.run(:after_create_fileset, fs, ::User.find_by(uid: fs.depositor)) }
   end
 
-  def file_set_entry_parents_present?(file_set_entries:)
-    file_set_entries&.all? { |fse| fse&.parsed_metadata&.[]('parent')&.present? }
+  def file_set_entry_parents_present?(fileset_entries:)
+    fileset_entries&.all? { |fse| fse&.parsed_metadata&.[]('parent')&.present? }
   end
 end
